6e46f49f27d066932da304fb867a87246917f7aa
sys_27 <sys2712@gmail.com>
2012-12-29 18:01:32 +0200

    Fix a license text in AboutView.

--- xFunc.App/Resources/Resource.Designer.cs
+++ xFunc.App/Resources/Resource.Designer.cs
@@ -61,6 +61,26 @@ namespace xFunc.App.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Copyright [yyyy] [name of copyright owner]
+        ///
+        ///   Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+        ///   you may not use this file except in compliance with the License.
+        ///   You may obtain a copy of the License at
+        ///
+        ///       http://www.apache.org/licenses/LICENSE-2.0
+        ///
+        ///   Unless required by applicable law or agreed to in writing, software
+        ///   distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+        ///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        ///   See the  [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string aboutLicense {
+            get {
+                return ResourceManager.GetString("aboutLicense", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to About.
         /// </summary>
         public static string aboutMenu {

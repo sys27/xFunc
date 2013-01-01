4fda30f4219ef02fe6167855b33d8131c83363ae
Dmitry <sys_27@mail.ru>
2013-01-01 16:39:24 +0200

    Added assignment value to the variable.

---
+++ xFunc.Library/LogicExpressionItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using xFunc.Library.Logics.Expressions;
+
+namespace xFunc.Library
+{
+
+    public class LogicExpressionItem
+    {
+
+        private ILogicExpression exp;
+        private string answer;
+
+        public LogicExpressionItem()
+        {
+
+        }
+
+        public LogicExpressionItem(ILogicExpression exp, string answer)
+        {
+            this.exp = exp;
+            this.answer = answer;
+        }
+
+        public ILogicExpression Expression
+        {
+            get
+            {
+                return exp;
+            }
+            set
+            {
+                exp = value;
+            }
+        }
+
+        public string Answer
+        {
+            get
+            {
+                return answer;
+            }
+            set
+            {
+                answer = value;
+            }
+        }
+
+    }
+
+}

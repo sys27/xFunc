102a49d33e600ebef5c2e958149408a62ba0d08a
sys_27 <sys2712@gmail.com>
2013-01-03 16:20:46 +0200

    Added tests to logic operations.

---
+++ xFunc.Test/Expressions/Logics/NotLogicExpressionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Library.Logics.Expressions;
+using xFunc.Library.Logics;
+
+namespace xFunc.Test.Expressions.Logics
+{
+
+    [TestClass]
+    public class NotLogicExpressionTest
+    {
+
+        private LogicParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new LogicParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            ILogicExpression exp = parser.Parse("!a");
+            LogicParameterCollection parameters = new LogicParameterCollection();
+            parameters.Add('a');
+
+            parameters['a'] = true;
+            Assert.IsFalse(exp.Calculate(parameters));
+
+            parameters['a'] = false;
+            Assert.IsTrue(exp.Calculate(parameters));
+        }
+
+    }
+
+}

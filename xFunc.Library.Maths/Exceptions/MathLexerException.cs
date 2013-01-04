d8542aab6835c63ae374f1b7a11a0a4083e4d732
Dmytro Kyshchenko <sys2712@gmail.com>
2013-01-04 14:38:09 +0200

    Create two separate libraries. One for maths, another for logics.

---
+++ xFunc.Library.Maths/Exceptions/MathLexerException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace xFunc.Library.Maths.Exceptions
+{
+
+    [Serializable]
+    public class MathLexerException : Exception
+    {
+
+        public MathLexerException() { }
+
+        public MathLexerException(string message) : base(message) { }
+        
+        public MathLexerException(string message, Exception inner) : base(message, inner) { }
+        
+        protected MathLexerException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    }
+
+}

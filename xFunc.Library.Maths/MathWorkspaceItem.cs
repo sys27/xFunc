c81826fc95303a7b4132176c45739b0328f51843
Dmytro Kyshchenko <sys2712@gmail.com>
2013-01-05 16:57:50 +0200

    Added MathWorkspace

---
+++ xFunc.Library.Maths/MathWorkspaceItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xFunc.Library.Maths.Expressions;
+
+namespace xFunc.Library.Maths
+{
+
+    public class MathWorkspaceItem
+    {
+
+        private string strExp;
+        private IMathExpression exp;
+        private string answer;
+
+        public MathWorkspaceItem()
+        {
+
+        }
+
+        public MathWorkspaceItem(string strExp, IMathExpression exp)
+            : this(strExp, exp, null)
+        {
+
+        }
+
+        public MathWorkspaceItem(string strExp, IMathExpression exp, string answer)
+        {
+            this.strExp = strExp;
+            this.exp = exp;
+            this.answer = answer;
+        }
+
+        public string StringExpression
+        {
+            get
+            {
+                return strExp;
+            }
+        }
+
+        public IMathExpression Expression
+        {
+            get
+            {
+                return exp;
+            }
+        }
+
+        public string Answer
+        {
+            get
+            {
+                return answer;
+            }
+            internal set
+            {
+                answer = value;
+            }
+        }
+
+    }
+
+}

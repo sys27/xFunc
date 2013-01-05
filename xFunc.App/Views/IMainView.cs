39cf36dab16b5b479f97b21da72055c5d6b693da
sys_27 <sys2712@gmail.com>
2013-01-05 22:05:55 +0200

    Added actions to some buttons. Added a data template to the math ListBox.

--- xFunc.App/Views/IMainView.cs
+++ xFunc.App/Views/IMainView.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using xFunc.Library.Maths;
 
 namespace xFunc.App.Views
 {
 
     public interface IMainView
     {
+
+        IEnumerable<MathWorkspaceItem> MathExpressions { set; }
 
     }
 

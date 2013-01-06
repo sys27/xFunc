19a8da315acc3951ec97635d3aa1cdbdab95ec17
Dmytro Kyshchenko <sys2712@gmail.com>
2013-01-06 16:51:38 +0200

    Add LogicWorkspace.

---
+++ xFunc.Library.Logics/LogicWorkspaceItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xFunc.Library.Logics.Expressions;
+
+namespace xFunc.Library.Logics
+{
+
+    public class LogicWorkspaceItem
+    {
+
+        private string strExp;
+        private ILogicExpression exp;
+        private string answer;
+
+        public LogicWorkspaceItem()
+        {
+
+        }
+
+        public LogicWorkspaceItem(string strExp, ILogicExpression exp, string answer)
+        {
+            this.strExp = strExp;
+            this.exp = exp;
+            this.answer = answer;
+        }
+
+        public string StringExpression
+        {
+            get
+            {
+                return strExp;
+            }
+        }
+
+        public ILogicExpression Expression
+        {
+            get
+            {
+                return exp;
+            }
+        }
+
+        public string Answer
+        {
+            get
+            {
+                return answer;
+            }
+        }
+
+    }
+
+}

0150c0d54ff899f11812c20ce08817b66815b1ae
sys_27 <sys_27@mail.ru>
2013-01-07 17:25:27 +0200

    Added sec, csc functions.

---
+++ xFunc.Library.Maths/Expressions/ArccscMathExpression.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.Library.Maths.Expressions
+{
+
+    public class ArccscMathExpression : TrigonometryMathExpression
+    {
+
+        public ArccscMathExpression() : base(null) { }
+
+        public ArccscMathExpression(IMathExpression firstMathExpression) : base(firstMathExpression) { }
+
+        public override string ToString()
+        {
+            return ToString("arccsc({0})");
+        }
+
+        public override double CalculateDergee(MathParameterCollection parameters)
+        {
+            var radian = 1 / firstMathExpression.Calculate(parameters);
+
+            return Math.Asin(radian) / Math.PI * 180;
+        }
+
+        public override double CalculateRadian(MathParameterCollection parameters)
+        {
+            return Math.Asin(1 / firstMathExpression.Calculate(parameters));
+        }
+
+        public override double CalculateGradian(MathParameterCollection parameters)
+        {
+            var radian = 1 / firstMathExpression.Calculate(parameters);
+
+            return Math.Asin(radian) / Math.PI * 200;
+        }
+
+        protected override IMathExpression _Derivative(VariableMathExpression variable)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ xFunc.Library.Maths/Expressions/SecantMathExpression.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.Library.Maths.Expressions
+{
+
+    public class SecantMathExpression : TrigonometryMathExpression
+    {
+
+        public SecantMathExpression()
+            : base(null)
+        {
+
+        }
+
+        public SecantMathExpression(IMathExpression firstMathExpression)
+            : base(firstMathExpression)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return ToString("sec({0})");
+        }
+
+        public override double CalculateDergee(MathParameterCollection parameters)
+        {
+            var radian = firstMathExpression.Calculate(parameters) * Math.PI / 180;
+
+            return 1 / Math.Cos(radian);
+        }
+
+        public override double CalculateRadian(MathParameterCollection parameters)
+        {
+            return 1 / Math.Cos(firstMathExpression.Calculate(parameters));
+        }
+
+        public override double CalculateGradian(MathParameterCollection parameters)
+        {
+            var radian = firstMathExpression.Calculate(parameters) * Math.PI / 200;
+
+            return 1 / Math.Cos(radian);
+        }
+
+        protected override IMathExpression _Derivative(VariableMathExpression variable)
+        {
+            TangentMathExpression tan = new TangentMathExpression(firstMathExpression);
+            SecantMathExpression sec = new SecantMathExpression(firstMathExpression);
+            MultiplicationMathExpression mul1 = new MultiplicationMathExpression(tan, sec);
+            MultiplicationMathExpression mul2 = new MultiplicationMathExpression(firstMathExpression.Derivative(variable), mul1);
+
+            return mul2;
+        }
+
+    }
+
+}

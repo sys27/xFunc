3ae7f67d5d135c0a55e5d1aecc2b66e8e0782a42
sys27 <sys2712@gmail.com>
2013-01-07 17:59:01 +0200

    Added derivatives for arcsec, arccsc.

--- xFunc.Library.Maths/MathExtentions.cs
+++ xFunc.Library.Maths/MathExtentions.cs
@@ -2,7 +2,7 @@
 
 namespace xFunc.Library.Maths
 {
-    
+
     public static class MathExtentions
     {
 
@@ -11,9 +11,29 @@ namespace xFunc.Library.Maths
             return Math.Cos(d) / Math.Sin(d);
         }
 
+        public static double Sec(double d)
+        {
+            return 1 / Math.Cos(d);
+        }
+
+        public static double Csc(double d)
+        {
+            return 1 / Math.Sin(d);
+        }
+
         public static double Acot(double d)
         {
             return Math.PI / 2 - Math.Atan(d);
+        }
+
+        public static double Asec(double d)
+        {
+            return Math.Acos(1 / d);
+        }
+
+        public static double Acsc(double d)
+        {
+            return Math.Asin(1 / d);
         }
 
     }
---
+++ xFunc.Test/Expressions/Maths/ArcsecMathExpressionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Library.Maths;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class ArcsecMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        // todo: ...
+
+    }
+
+}

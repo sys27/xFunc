8acd15b14855bae5b078417ca594df9787feb2d6
sys27 <sys2712@gmail.com>
2013-01-23 14:26:30 +0200

    #4 Added sinh, cosh.

---
+++ xFunc.Test/Expressions/Maths/HyperbolicSineMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicSineMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("sinh(1)");
+
+            Assert.AreEqual(Math.Sinh(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(sinh(2x), x)").Derivative();
+
+            Assert.AreEqual("2 * cosh(2 * x)", exp.ToString());
+        }
+
+    }
+
+}

9acb622021caadc2bd65c92f0e04ae07272e1616
sys_27 <sys2712@gmail.com>
2013-01-24 12:14:49 +0200

    #4 Added arcosh.

---
+++ xFunc.Test/Expressions/Maths/HyperbolicArcosineMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicArcosineMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("arcosh(1)");
+
+            Assert.AreEqual(MathExtentions.Acosh(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(arcosh(2x), x)").Derivative();
+
+            Assert.AreEqual("2 / sqrt(((2 * x) ^ 2) - 1)", exp.ToString());
+        }
+
+    }
+
+}

55f964869727b8043faa45f35725c6132d26e734
Dmitry <sys_27@mail.ru>
2013-01-24 13:02:56 +0200

    #4 Added arsech, arcsch.

--- xFunc.Maths/Expressions/HyperbolicArcosecantMathExpression.cs
+++ xFunc.Maths/Expressions/HyperbolicArcosecantMathExpression.cs
@@ -20,16 +20,16 @@ using System.Text;
 namespace xFunc.Maths.Expressions
 {
 
-    public class HyperbolicArtangentMathExpression : UnaryMathExpression
+    public class HyperbolicArcosecantMathExpression : UnaryMathExpression
     {
 
-        public HyperbolicArtangentMathExpression()
+        public HyperbolicArcosecantMathExpression()
             : base(null)
         {
 
         }
 
-        public HyperbolicArtangentMathExpression(IMathExpression firstMathExpression)
+        public HyperbolicArcosecantMathExpression(IMathExpression firstMathExpression)
             : base(firstMathExpression)
         {
 
@@ -37,26 +37,30 @@ namespace xFunc.Maths.Expressions
 
         public override string ToString()
         {
-            return ToString("artanh({0})");
+            return ToString("arcsch({0})");
         }
 
         public override double Calculate(MathParameterCollection parameters)
         {
-            return MathExtentions.Atanh(firstMathExpression.Calculate(parameters));
+            return MathExtentions.Acsch(firstMathExpression.Calculate(parameters));
         }
 
         public override IMathExpression Clone()
         {
-            return new HyperbolicArtangentMathExpression(firstMathExpression.Clone());
+            return new HyperbolicArcosecantMathExpression(firstMathExpression.Clone());
         }
 
         protected override IMathExpression _Derivative(VariableMathExpression variable)
         {
-            var sqr = new ExponentiationMathExpression(firstMathExpression.Clone(), new NumberMathExpression(2));
-            var sub = new SubtractionMathExpression(new NumberMathExpression(1), sqr);
-            var div = new DivisionMathExpression(firstMathExpression.Clone().Derivative(variable), sub);
+            var inv = new ExponentiationMathExpression(firstMathExpression.Clone(), new NumberMathExpression(2));
+            var add = new AdditionMathExpression(new NumberMathExpression(1), inv);
+            var sqrt = new SqrtMathExpression(add);
+            var abs = new AbsoluteMathExpression(firstMathExpression.Clone());
+            var mul = new MultiplicationMathExpression(abs, sqrt);
+            var div = new DivisionMathExpression(firstMathExpression.Clone().Derivative(variable), mul);
+            var unMinus = new UnaryMinusMathExpression(div);
 
-            return div;
+            return unMinus;
         }
 
     }

a8c7dfa1215eaa6b05f85939331e4441ab136331
sys27 <sys2712@gmail.com>
2013-01-24 11:24:20 +0200

    #4 Added sech, csch.

---
+++ xFunc.Test/Expressions/Maths/HyperbolicCosecantMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicCosecantMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("csch(1)");
+
+            Assert.AreEqual(MathExtentions.Csch(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(csch(2x), x)").Derivative();
+
+            Assert.AreEqual("-(2 * (coth(2 * x) * csch(2 * x)))", exp.ToString());
+        }
+
+    }
+
+}
---
+++ xFunc.Test/Expressions/Maths/HyperbolicSecantMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicSecantMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("sech(1)");
+
+            Assert.AreEqual(MathExtentions.Sech(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(sech(2x), x)").Derivative();
+
+            Assert.AreEqual("-(2 * (tanh(2 * x) * sech(2 * x)))", exp.ToString());
+        }
+
+    }
+
+}

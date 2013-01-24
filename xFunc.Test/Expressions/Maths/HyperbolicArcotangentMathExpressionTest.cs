9fd5c4241c7c8bc4fc79d38e4038c13ef789c3d4
sys27 <sys2712@gmail.com>
2013-01-24 12:50:43 +0200

    #4 Added arcoth.

---
+++ xFunc.Test/Expressions/Maths/HyperbolicArcotangentMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicArcotangentMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("arcoth(1)");
+
+            Assert.AreEqual(MathExtentions.Acoth(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(arcoth(2x), x)").Derivative();
+
+            Assert.AreEqual("2 / (1 - ((2 * x) ^ 2))", exp.ToString());
+        }
+
+    }
+
+}

dade59115a564bbcb6060ea47edf8a9ac280786c
Dmytro Kyshchenko <sys2712@gmail.com>
2013-01-24 12:44:58 +0200

    #4 Added artanh.

---
+++ xFunc.Test/Expressions/Maths/HyperbolicArtangentMathExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class HyperbolicArtangentMathExpressionTest
+    {
+
+        private MathParser parser;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            parser = new MathParser();
+        }
+
+        [TestMethod]
+        public void CalculateTest()
+        {
+            var exp = parser.Parse("artanh(1)");
+
+            Assert.AreEqual(MathExtentions.Atanh(1), exp.Calculate(null));
+        }
+
+        [TestMethod]
+        public void DerivativeTest()
+        {
+            IMathExpression exp = parser.Parse("deriv(artanh(2x), x)").Derivative();
+
+            Assert.AreEqual("2 / (1 - ((2 * x) ^ 2))", exp.ToString());
+        }
+
+    }
+
+}

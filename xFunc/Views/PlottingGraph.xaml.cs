4bbf204f682ffcdbbaa36772eeef6f2542c5e98f
sys_27 <sys2712@gmail.com>
2013-01-28 22:33:41 +0200

    Fix the rendering of the grid.

--- xFunc/Views/PlottingGraph.xaml.cs
+++ xFunc/Views/PlottingGraph.xaml.cs
@@ -162,22 +162,60 @@ namespace xFunc.Views
                 Pen pen = new Pen(Brushes.Blue, 0.5);
                 using (DrawingContext context = gridVisual.RenderOpen())
                 {
-                    for (double x = centerX; x >= 0; x -= cm)
+                    if (centerX < 0)
                     {
-                        context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        for (double x = centerX % cm; x <= currentWidth; x += cm)
+                        {
+                            if (x >= 0)
+                                context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        }
                     }
-                    for (double x = centerX; x <= currentWidth; x += cm)
+                    else if (centerX > currentWidth)
                     {
-                        context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        for (double x = currentWidth + ((centerX - currentWidth) % cm); x >= 0; x -= cm)
+                        {
+                            if (x <= currentWidth)
+                                context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        }
+                    }
+                    else
+                    {
+                        for (double x = centerX; x >= 0; x -= cm)
+                        {
+                            context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        }
+                        for (double x = centerX; x <= currentWidth; x += cm)
+                        {
+                            context.DrawLine(pen, new Point(x, 0), new Point(x, currentHeight));
+                        }
                     }
 
-                    for (double y = centerY; y >= 0; y -= cm)
+                    if (centerY < 0)
                     {
-                        context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        for (double y = centerY % cm; y <= currentHeight; y += cm)
+                        {
+                            if (y >= 0)
+                                context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        }
                     }
-                    for (double y = centerY; y <= currentHeight; y += cm)
+                    else if (centerY > currentHeight)
                     {
-                        context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        for (double y = currentHeight + ((centerY - currentHeight) % cm); y >= 0; y -= cm)
+                        {
+                            if (y <= currentHeight)
+                                context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        }
+                    }
+                    else
+                    {
+                        for (double y = centerY; y >= 0; y -= cm)
+                        {
+                            context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        }
+                        for (double y = centerY; y <= currentHeight; y += cm)
+                        {
+                            context.DrawLine(pen, new Point(0, y), new Point(currentWidth, y));
+                        }
                     }
                 }
 
@@ -195,12 +233,7 @@ namespace xFunc.Views
                 context.DrawLine(pen, new Point(centerX, 0), new Point(centerX, currentHeight));
                 context.DrawLine(pen, new Point(0, centerY), new Point(currentWidth, centerY));
                 if (slider.Value <= 1.5)
-                    context.DrawText(new FormattedText("0",
-                                                       this.Dispatcher.Thread.CurrentCulture,
-                                                       FlowDirection.LeftToRight,
-                                                       new Typeface("Arial"), 10,
-                                                       Brushes.Black),
-                                     new Point(centerX - 12, centerY + 7));
+                    context.DrawText(new FormattedText("0", this.Dispatcher.Thread.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(centerX - 12, centerY + 7));
 
                 // OX
                 for (double x = centerX - cm; x >= 0; x -= cm)
@@ -208,24 +241,14 @@ namespace xFunc.Views
                     context.DrawLine(pen, new Point(x, centerY - 5), new Point(x, centerY + 5));
 
                     if (slider.Value <= 1.5)
-                        context.DrawText(new FormattedText(Math.Round(-(centerX - x) / cm, 0).ToString(),
-                                                           this.Dispatcher.Thread.CurrentCulture,
-                                                           FlowDirection.LeftToRight,
-                                                           new Typeface("Arial"), 10,
-                                                           Brushes.Black),
-                                         new Point(x - 15, centerY + 7));
+                        context.DrawText(new FormattedText(Math.Round(-(centerX - x) / cm, 0).ToString(), this.Dispatcher.Thread.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(x - 15, centerY + 7));
                 }
                 for (double x = centerX + cm; x <= currentWidth; x += cm)
                 {
                     context.DrawLine(pen, new Point(x, centerY - 5), new Point(x, centerY + 5));
 
                     if (slider.Value <= 1.5)
-                        context.DrawText(new FormattedText(Math.Round((x - centerX) / cm, 0).ToString(),
-                                                           this.Dispatcher.Thread.CurrentCulture,
-                                                           FlowDirection.LeftToRight,
-                                                           new Typeface("Arial"), 10,
-                                                           Brushes.Black),
-                                         new Point(x - 12, centerY + 7));
+                        context.DrawText(new FormattedText(Math.Round((x - centerX) / cm, 0).ToString(), this.Dispatcher.Thread.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(x - 12, centerY + 7));
                 }
 
                 // OY
@@ -234,24 +257,14 @@ namespace xFunc.Views
                     context.DrawLine(pen, new Point(centerX - 5, y), new Point(centerX + 5, y));
 
                     if (slider.Value <= 1.5)
-                        context.DrawText(new FormattedText(Math.Round((centerY - y) / cm, 0).ToString(),
-                                                           this.Dispatcher.Thread.CurrentCulture,
-                                                           FlowDirection.LeftToRight,
-                                                           new Typeface("Arial"),
-                                                           10, Brushes.Black),
-                                         new Point(centerX - 12, y + 7));
+                        context.DrawText(new FormattedText(Math.Round((centerY - y) / cm, 0).ToString(), this.Dispatcher.Thread.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(centerX - 12, y + 7));
                 }
                 for (double y = centerY + cm; y <= currentHeight; y += cm)
                 {
                     context.DrawLine(pen, new Point(centerX - 5, y), new Point(centerX + 5, y));
 
                     if (slider.Value <= 1.5)
-                        context.DrawText(new FormattedText(Math.Round(-(y - centerY) / cm, 0).ToString(),
-                                                           this.Dispatcher.Thread.CurrentCulture,
-                                                           FlowDirection.LeftToRight,
-                                                           new Typeface("Arial"), 10,
-                                                           Brushes.Black),
-                                         new Point(centerX - 15, y + 7));
+                        context.DrawText(new FormattedText(Math.Round(-(y - centerY) / cm, 0).ToString(), this.Dispatcher.Thread.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Arial"), 10, Brushes.Black), new Point(centerX - 15, y + 7));
                 }
             }
             canvas.AddVisual(oxoyVisual);

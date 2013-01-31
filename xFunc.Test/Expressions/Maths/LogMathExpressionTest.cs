67b31ce8f470ba14ef8e2f7e6018590f3e65c1d8
Dmytro Kyshchenko <sys2712@gmail.com>
2013-01-31 10:47:54 +0200

    Improved the simplification.

--- xFunc.Test/Expressions/Maths/LogMathExpressionTest.cs
+++ xFunc.Test/Expressions/Maths/LogMathExpressionTest.cs
@@ -66,6 +66,15 @@ namespace xFunc.Test.Expressions.Maths
         }
 
         [TestMethod]
+        public void SimplifyTest()
+        {
+            IMathExpression exp = parser.Parse("log(4x, 4x)");
+            NumberMathExpression one = 1;
+
+            Assert.AreEqual(one, exp);
+        }
+
+        [TestMethod]
         public void ToStringTest()
         {
             IMathExpression exp = parser.Parse("log(10, 2)");

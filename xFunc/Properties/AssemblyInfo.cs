85f72a6474ab5fda5edc6bf717b9b8d0985c395d
sys_27 <sys2712@gmail.com>
2013-02-09 19:24:24 +0200

    Adding tests.

--- xFunc/Properties/AssemblyInfo.cs
+++ xFunc/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]

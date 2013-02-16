ec671db87be909a56e50ba3d5044061fece3052a
sys_27 <sys_27@mail.ru>
2013-02-16 14:52:09 +0200

    Edited Division tests.

--- xFunc.Test/Expressions/Maths/DivisionTest.cs
+++ xFunc.Test/Expressions/Maths/DivisionTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using xFunc.Maths;
 using xFunc.Maths.Expressions;
+using xFunc.Maths.Expressions.Trigonometric;
 
 namespace xFunc.Test.Expressions.Maths
 {
@@ -10,40 +11,35 @@ namespace xFunc.Test.Expressions.Maths
     public class DivisionTest
     {
 
-        private MathParser parser;
-
-        [TestInitialize]
-        public void TestInit()
-        {
-            parser = new MathParser();
-        }
-
         [TestMethod]
         public void CalculateTest()
         {
-            IMathExpression exp = parser.Parse("1 / 2");
+            IMathExpression exp = new Division(new Number(1), new Number(2));
 
             Assert.AreEqual(1.0 / 2.0, exp.Calculate(null));
         }
 
         [TestMethod]
-        public void DerivativeTest()
-        {
-            IMathExpression exp = MathParser.Differentiation(parser.Parse("1 / x"));
-
-            Assert.AreEqual("-1 / (x ^ 2)", exp.ToString());
-        }
-
-        [TestMethod]
         public void DerivativeTest1()
         {
-            IMathExpression exp = MathParser.Differentiation(parser.Parse("sin(x) / x"));
+            IMathExpression exp = new Division(new Number(1), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("((cos(x) * x) - sin(x)) / (x ^ 2)", exp.ToString());
+            Assert.AreEqual("-(1 * 1) / (x ^ 2)", deriv.ToString());
         }
 
         [TestMethod]
         public void DerivativeTest2()
+        {
+            // sin(x) / x
+            IMathExpression exp = new Division(new Sine(new Variable('x')), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation();
+
+            Assert.AreEqual("(((cos(x) * 1) * x) - (sin(x) * 1)) / (x ^ 2)", deriv.ToString());
+        }
+
+        [TestMethod]
+        public void DerivativeTest3()
         {
             // (2x) / (3x)
             Number num1 = new Number(2);
@@ -54,48 +50,54 @@ namespace xFunc.Test.Expressions.Maths
             Multiplication mul2 = new Multiplication(num2, x.Clone());
 
             IMathExpression exp = new Division(mul1, mul2);
-            IMathExpression deriv = MathParser.Differentiation(exp);
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("((6 * x) - (6 * x)) / ((3 * x) ^ 2)", deriv.ToString());
+            Assert.AreEqual("(((2 * 1) * (3 * x)) - ((2 * x) * (3 * 1))) / ((3 * x) ^ 2)", deriv.ToString());
 
             num1.Value = 4;
             num2.Value = 5;
             Assert.AreEqual("(4 * x) / (5 * x)", exp.ToString());
-            Assert.AreEqual("((6 * x) - (6 * x)) / ((3 * x) ^ 2)", deriv.ToString());
+            Assert.AreEqual("(((2 * 1) * (3 * x)) - ((2 * x) * (3 * 1))) / ((3 * x) ^ 2)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest1()
         {
-            IMathExpression exp = parser.Parse("deriv((y + x ^ 2) / x, x)").Differentiation();
-            Assert.AreEqual("(((2 * x) * x) - (y + (x ^ 2))) / (x ^ 2)", exp.ToString());
+            // (y + x ^ 2) / x
+            IMathExpression exp = new Division(new Addition(new Variable('y'), new Exponentiation(new Variable('x'), new Number(2))), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("(((1 * (2 * (x ^ (2 - 1)))) * x) - ((y + (x ^ 2)) * 1)) / (x ^ 2)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest2()
         {
-            IMathExpression exp = parser.Parse("deriv(y / x, x)").Differentiation();
-            Assert.AreEqual("-y / (x ^ 2)", exp.ToString());
+            IMathExpression exp = new Division(new Variable('y'), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("-(y * 1) / (x ^ 2)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest3()
         {
-            IMathExpression exp = parser.Parse("deriv(y / x, y)").Differentiation();
-            Assert.AreEqual("1 / x", exp.ToString());
+            IMathExpression exp = new Division(new Variable('y'), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation(new Variable('y'));
+            Assert.AreEqual("1 / x", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest4()
         {
-            IMathExpression exp = parser.Parse("deriv((x + 1) / x, y)").Differentiation();
-            Assert.AreEqual("0", exp.ToString());
+            // (x + 1) / x
+            IMathExpression exp = new Division(new Addition(new Variable('x'), new Number(1)), new Variable('x'));
+            IMathExpression deriv = exp.Differentiation(new Variable('y'));
+            Assert.AreEqual("0", deriv.ToString());
         }
 
         [TestMethod]
         public void ToStringTest()
         {
-            IMathExpression exp = parser.Parse("1 / 2");
+            IMathExpression exp = new Division(new Number(1), new Number(2));
 
             Assert.AreEqual("1 / 2", exp.ToString());
         }

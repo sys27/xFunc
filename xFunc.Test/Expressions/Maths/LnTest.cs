bbd7e3543413095edf5b30a15c1dfdec6dafe62e
sys27 <sys2712@gmail.com>
2013-02-17 09:45:09 +0200

    Edited Ln tests

--- xFunc.Test/Expressions/Maths/LnTest.cs
+++ xFunc.Test/Expressions/Maths/LnTest.cs
@@ -10,28 +10,21 @@ namespace xFunc.Test.Expressions.Maths
     public class LnTest
     {
 
-        private MathParser parser;
-
-        [TestInitialize]
-        public void TestInit()
-        {
-            parser = new MathParser();
-        }
-
         [TestMethod]
         public void CalculateTest()
         {
-            IMathExpression exp = parser.Parse("ln(2)");
+            IMathExpression exp = new Ln(new Number(2));
 
-            Assert.AreEqual(Math.Log(2, Math.E), exp.Calculate(null));
+            Assert.AreEqual(Math.Log(2), exp.Calculate(null));
         }
 
         [TestMethod]
         public void DerivativeTest1()
         {
-            IMathExpression exp = MathParser.Differentiation(parser.Parse("ln(2x)"));
+            IMathExpression exp = new Ln(new Multiplication(new Number(2), new Variable('x')));
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("1 / x", exp.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * x)", deriv.ToString());
         }
 
         [TestMethod]
@@ -43,42 +36,39 @@ namespace xFunc.Test.Expressions.Maths
             Multiplication mul = new Multiplication(num, x);
 
             IMathExpression exp = new Ln(mul);
-            IMathExpression deriv = MathParser.Differentiation(exp);
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("1 / x", deriv.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * x)", deriv.ToString());
 
             num.Value = 5;
             Assert.AreEqual("ln(5 * x)", exp.ToString());
-            Assert.AreEqual("1 / x", deriv.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * x)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest1()
         {
-            IMathExpression exp = parser.Parse("deriv(ln(xy), x)").Differentiation();
-            Assert.AreEqual("y / (x * y)", exp.ToString());
+            // ln(xy)
+            IMathExpression exp = new Ln(new Multiplication(new Variable('x'), new Variable('y')));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("(1 * y) / (x * y)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest2()
         {
-            IMathExpression exp = parser.Parse("deriv(ln(xy), y)").Differentiation();
-            Assert.AreEqual("x / (x * y)", exp.ToString());
+            // ln(xy)
+            IMathExpression exp = new Ln(new Multiplication(new Variable('x'), new Variable('y')));
+            IMathExpression deriv = exp.Differentiation(new Variable('y'));
+            Assert.AreEqual("(x * 1) / (x * y)", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest3()
         {
-            IMathExpression exp = parser.Parse("deriv(ln(y), x)").Differentiation();
-            Assert.AreEqual("0", exp.ToString());
-        }
-
-        [TestMethod]
-        public void ToStringTest()
-        {
-            IMathExpression exp = parser.Parse("ln(2)");
-
-            Assert.AreEqual("ln(2)", exp.ToString());
+            IMathExpression exp = new Ln(new Variable('y'));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("0", deriv.ToString());
         }
 
     }

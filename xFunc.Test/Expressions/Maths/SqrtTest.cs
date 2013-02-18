a8149947d9e09cfc6c8fbfa7d9e7abce7bbf677c
sys27 <sys2712@gmail.com>
2013-02-18 20:25:14 +0200

    Edited Sqrt tests

--- xFunc.Test/Expressions/Maths/SqrtTest.cs
+++ xFunc.Test/Expressions/Maths/SqrtTest.cs
@@ -10,18 +10,10 @@ namespace xFunc.Test.Expressions.Maths
     public class SqrtTest
     {
 
-        private MathParser parser;
-
-        [TestInitialize]
-        public void TestInit()
-        {
-            parser = new MathParser();
-        }
-
         [TestMethod]
         public void CalculateTest()
         {
-            IMathExpression exp = parser.Parse("sqrt(4)");
+            IMathExpression exp = new Sqrt(new Number(4));
 
             Assert.AreEqual(Math.Sqrt(4), exp.Calculate(null));
         }
@@ -29,9 +21,10 @@ namespace xFunc.Test.Expressions.Maths
         [TestMethod]
         public void DerivativeTest1()
         {
-            IMathExpression exp = MathParser.Differentiation(parser.Parse("sqrt(2x)"));
+            IMathExpression exp = new Sqrt(new Multiplication(new Number(2), new Variable('x')));
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("1 / sqrt(2 * x)", exp.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * sqrt(2 * x))", deriv.ToString());
         }
 
         [TestMethod]
@@ -42,35 +35,30 @@ namespace xFunc.Test.Expressions.Maths
             Multiplication mul = new Multiplication(num, x);
 
             IMathExpression exp = new Sqrt(mul);
-            IMathExpression deriv = MathParser.Differentiation(exp);
+            IMathExpression deriv = exp.Differentiation();
 
-            Assert.AreEqual("1 / sqrt(2 * x)", deriv.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * sqrt(2 * x))", deriv.ToString());
 
             num.Value = 3;
             Assert.AreEqual("sqrt(3 * x)", exp.ToString());
-            Assert.AreEqual("1 / sqrt(2 * x)", deriv.ToString());
+            Assert.AreEqual("(2 * 1) / (2 * sqrt(2 * x))", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest1()
         {
-            IMathExpression exp = parser.Parse("deriv(sqrt(2xy), x)").Differentiation();
-            Assert.AreEqual("(2 * y) / (2 * sqrt((2 * x) * y))", exp.ToString());
+            // sqrt(2xy)
+            IMathExpression exp = new Sqrt(new Multiplication(new Multiplication(new Number(2), new Variable('x')), new Variable('y')));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("((2 * 1) * y) / (2 * sqrt((2 * x) * y))", deriv.ToString());
         }
 
         [TestMethod]
         public void PartialDerivativeTest2()
         {
-            IMathExpression exp = parser.Parse("deriv(sqrt(y), x)").Differentiation();
-            Assert.AreEqual("0", exp.ToString());
-        }
-
-        [TestMethod]
-        public void ToStringTest()
-        {
-            IMathExpression exp = parser.Parse("sqrt(8)");
-
-            Assert.AreEqual("sqrt(8)", exp.ToString());
+            IMathExpression exp = new Sqrt(new Variable('y'));
+            IMathExpression deriv = exp.Differentiation();
+            Assert.AreEqual("0", deriv.ToString());
         }
 
     }

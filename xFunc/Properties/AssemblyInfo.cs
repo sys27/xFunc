8454b96520f44115e2aa18b5401d46514861e61f
sys_27 <sys_27@mail.ru>
2013-02-22 21:54:37 +0200

    Optimized the plotting.

--- xFunc/Properties/AssemblyInfo.cs
+++ xFunc/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.0.2.3")]
-[assembly: AssemblyFileVersion("2.0.2.3")]
+[assembly: AssemblyVersion("2.0.2.4")]
+[assembly: AssemblyFileVersion("2.0.2.4")]

862d2fa2bc84134c9ccff093017f7671bcb7bd8c
sys_27 <sys2712@gmail.com>
2013-03-01 18:29:52 +0200

    Small fixes

--- xFunc.Test/MathParserTest.cs
+++ xFunc.Test/MathParserTest.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using xFunc.Maths;
 using xFunc.Maths.Expressions;
 using xFunc.Maths.Expressions.Trigonometric;
+using xFunc.Maths.Exceptions;
+using System.Collections.Generic;
 
 namespace xFunc.Test
 {
@@ -44,6 +46,159 @@ namespace xFunc.Test
         public void ParseNullStr()
         {
             parser.Parse(null);
+        }
+
+        [TestMethod]
+        public void ParseLog()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Log),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken(9),
+                new MathToken(MathTokenType.Comma),
+                new MathToken(3),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("log(9, 3)", false);
+            Assert.AreEqual("log(9, 3)", exp.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseLogWithOneParam()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Log),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken(9),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("log(9)");
+        }
+
+        [TestMethod]
+        public void ParseRoot()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Root),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken('x'),
+                new MathToken(MathTokenType.Comma),
+                new MathToken(3),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("root(x, 3)", false);
+            Assert.AreEqual("root(x, 3)", exp.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseRootWithOneParam()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Root),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken('x'),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("root(x)");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseDerivWithOneParam()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Derivative),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken('x'),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("deriv(x)");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseDerivSecondParamIsNotVar()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Derivative),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken('x'),
+                new MathToken(MathTokenType.Comma),
+                new MathToken(3),
+                new MathToken(MathTokenType.CloseBracket)
+            };
+
+            var exp = parser.Parse("deriv(x, 3)");
+        }
+
+        [TestMethod]
+        public void ParseAssign()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken('x'),
+                new MathToken(MathTokenType.Assign),
+                new MathToken(3)
+            };
+
+            var exp = parser.Parse("x := 3", false);
+            Assert.AreEqual("x := 3", exp.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseAssignWithOneParam()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken('x'),
+                new MathToken(MathTokenType.Assign)
+            };
+
+            var exp = parser.Parse("x := ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ParseAssignFirstParamIsNotVar()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(5),
+                new MathToken(MathTokenType.Assign),
+                new MathToken(3)
+            };
+
+            var exp = parser.Parse("5 := 3");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MathParserException))]
+        public void ErrorWhileParsingTree()
+        {
+            lexer.Tokens = new List<MathToken>()
+            {
+                new MathToken(MathTokenType.Sine),
+                new MathToken(MathTokenType.OpenBracket),
+                new MathToken('x'),
+                new MathToken(MathTokenType.CloseBracket),
+                new MathToken(2)
+            };
+
+            var exp = parser.Parse("sin(x) 2");
         }
 
     }

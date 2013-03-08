0046a24a783515712f9a498c88c2924bc863a708
sys27 <sys2712@gmail.com>
2013-03-08 21:15:44 +0200

    #21 Adding XML Documentation.

--- xFunc.Maths/Expressions/Bitwise/Or.cs
+++ xFunc.Maths/Expressions/Bitwise/Or.cs
@@ -3,21 +3,37 @@
 namespace xFunc.Maths.Expressions.Bitwise
 {
 
+    /// <summary>
+    /// Represents a bitwise OR operation.
+    /// </summary>
     public class Or : BinaryMathExpression
     {
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Or"/> class.
+        /// </summary>
         public Or()
             : base(null, null)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Or"/> class.
+        /// </summary>
+        /// <param name="firstMathExpression">The left operand.</param>
+        /// <param name="secondMathExpression">The right operand.</param>
+        /// <seealso cref="IMathExpression"/>
         public Or(IMathExpression firstMathExpression, IMathExpression secondMathExpression)
             : base(firstMathExpression, secondMathExpression)
         {
 
         }
 
+        /// <summary>
+        /// Converts this expression to the equivalent string.
+        /// </summary>
+        /// <returns>The string that represents this expression.</returns>
         public override string ToString()
         {
             if (parentMathExpression is BinaryMathExpression)
@@ -27,17 +43,32 @@ namespace xFunc.Maths.Expressions.Bitwise
 
             return ToString("{0} or {1}");
         }
-        
+
+        /// <summary>
+        /// Calculates this bitwise OR expression.
+        /// </summary>
+        /// <param name="parameters">A collection of variables that are used in the expression.</param>
+        /// <returns>A result of the calculation.</returns>
         public override double Calculate(MathParameterCollection parameters)
         {
             return (int)firstMathExpression.Calculate(parameters) | (int)secondMathExpression.Calculate(parameters);
         }
 
+        /// <summary>
+        /// Clones this instanse of the <see cref="Or"/>.
+        /// </summary>
+        /// <returns>Returns the new instance of <see cref="IMathExpression"/> that is a clone of this instance.</returns>
         public override IMathExpression Clone()
         {
             return new Or(firstMathExpression.Clone(), secondMathExpression.Clone());
         }
 
+        /// <summary>
+        /// This method always throws the <see cref="NotSupportedException"/> exception.
+        /// </summary>
+        /// <param name="variable">This method always throws the <see cref="NotSupportedException"/> exception.</param>
+        /// <returns>This method always throws the <see cref="NotSupportedException"/> exception.</returns>
+        /// <exception cref="NotSupportedException">Always is thrown.</exception>
         public override IMathExpression Differentiation(Variable variable)
         {
             throw new NotSupportedException();
--- xFunc.Maths/Expressions/Bitwise/XOr.cs
+++ xFunc.Maths/Expressions/Bitwise/XOr.cs
@@ -3,21 +3,37 @@
 namespace xFunc.Maths.Expressions.Bitwise
 {
 
+    /// <summary>
+    /// Represents a bitwise XOR operation.
+    /// </summary>
     public class XOr : BinaryMathExpression
     {
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XOr"/> class.
+        /// </summary>
         public XOr()
             : base(null, null)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XOr"/> class.
+        /// </summary>
+        /// <param name="firstMathExpression">The left operand.</param>
+        /// <param name="secondMathExpression">The right operand.</param>
+        /// <seealso cref="IMathExpression"/>
         public XOr(IMathExpression firstMathExpression, IMathExpression secondMathExpression)
             : base(firstMathExpression, secondMathExpression)
         {
 
         }
 
+        /// <summary>
+        /// Converts this expression to the equivalent string.
+        /// </summary>
+        /// <returns>The string that represents this expression.</returns>
         public override string ToString()
         {
             if (parentMathExpression is BinaryMathExpression)
@@ -27,17 +43,32 @@ namespace xFunc.Maths.Expressions.Bitwise
 
             return ToString("{0} xor {1}");
         }
-        
+
+        /// <summary>
+        /// Calculates this bitwise XOR expression.
+        /// </summary>
+        /// <param name="parameters">A collection of variables that are used in the expression.</param>
+        /// <returns>A result of the calculation.</returns>
         public override double Calculate(MathParameterCollection parameters)
         {
             return (int)firstMathExpression.Calculate(parameters) ^ (int)secondMathExpression.Calculate(parameters);
         }
 
+        /// <summary>
+        /// Clones this instanse of the <see cref="XOr"/>.
+        /// </summary>
+        /// <returns>Returns the new instance of <see cref="IMathExpression"/> that is a clone of this instance.</returns>
         public override IMathExpression Clone()
         {
             return new XOr(firstMathExpression.Clone(), secondMathExpression.Clone());
         }
 
+        /// <summary>
+        /// This method always throws the <see cref="NotSupportedException"/> exception.
+        /// </summary>
+        /// <param name="variable">This method always throws the <see cref="NotSupportedException"/> exception.</param>
+        /// <returns>This method always throws the <see cref="NotSupportedException"/> exception.</returns>
+        /// <exception cref="NotSupportedException">Always is thrown.</exception>
         public override IMathExpression Differentiation(Variable variable)
         {
             throw new NotSupportedException();

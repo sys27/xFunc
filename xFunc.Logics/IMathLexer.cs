94201b0657a1c6a6089ece227355e75c733a1e05
sys_27 <sys2712@gmail.com>
2013-03-13 15:30:18 +0200

    #21 Adding XML Documentation.

--- xFunc.Logics/IMathLexer.cs
+++ xFunc.Logics/IMathLexer.cs
@@ -7,6 +7,11 @@ namespace xFunc.Logics
     public interface IMathLexer
     {
 
+        /// <summary>
+        /// Converts the string into a sequence of tokens.
+        /// </summary>
+        /// <param name="function">The string that contains the functions and operators.</param>
+        /// <returns>The sequence of tokens.</returns>
         IEnumerable<LogicToken> Tokenize(string function);
 
     }

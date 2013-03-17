cf3850596b1a243098ee5f5f732e7415f34bbd84
sys_27 <sys2712@gmail.com>
2013-03-17 10:09:50 +0200

    #15

---
+++ xFunc/ViewModels/GraphItemViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.ViewModels
+{
+
+    public class GraphItemViewModel
+    {
+
+        private bool isChecked;
+        private IMathExpression exp;
+
+        public GraphItemViewModel(IMathExpression exp, bool isChecked)
+        {
+            this.exp = exp;
+            this.isChecked = isChecked;
+        }
+
+        public override string ToString()
+        {
+            return exp.ToString();
+        }
+
+        public bool IsChecked
+        {
+            get
+            {
+                return isChecked;
+            }
+            set
+            {
+                isChecked = value;
+            }
+        }
+
+        public IMathExpression Expression
+        {
+            get
+            {
+                return exp;
+            }
+        }
+
+    }
+
+}

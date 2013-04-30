e07576a8326fcc604beba3e9b28eae1f1c2059d6
sys_27 <sys_27@mail.ru>
2013-04-30 16:54:31 +0300

    Added funcs

--- xFunc.Maths/Tokens/Functions.cs
+++ xFunc.Maths/Tokens/Functions.cs
@@ -2,9 +2,161 @@
 
 namespace xFunc.Maths.Tokens
 {
-    
+
     public enum Functions
     {
+
+        /// <summary>
+        /// undef()
+        /// </summary>
+        Undefine,
+        /// <summary>
+        /// abs
+        /// </summary>
+        Absolute,
+        /// <summary>
+        /// sin
+        /// </summary>
+        Sine,
+        /// <summary>
+        /// cos
+        /// </summary>
+        Cosine,
+        /// <summary>
+        /// tg
+        /// </summary>
+        Tangent,
+        /// <summary>
+        /// Cot
+        /// </summary>
+        Cotangent,
+        /// <summary>
+        /// sec
+        /// </summary>
+        Secant,
+        /// <summary>
+        /// csc
+        /// </summary>
+        Cosecant,
+        /// <summary>
+        /// Arcsin
+        /// </summary>
+        Arcsine,
+        /// <summary>
+        /// Arccos
+        /// </summary>
+        Arccosine,
+        /// <summary>
+        /// Arctan
+        /// </summary>
+        Arctangent,
+        /// <summary>
+        /// Arccot
+        /// </summary>
+        Arccotangent,
+        /// <summary>
+        /// arcsec
+        /// </summary>
+        Arcsecant,
+        /// <summary>
+        /// arccsc
+        /// </summary>
+        Arccosecant,
+        /// <summary>
+        /// sqrt
+        /// </summary>
+        Sqrt,
+        /// <summary>
+        /// root
+        /// </summary>
+        Root,
+        /// <summary>
+        /// Ln
+        /// </summary>
+        Ln,
+        /// <summary>
+        /// Lg
+        /// </summary>
+        Lg,
+        /// <summary>
+        /// Log
+        /// </summary>
+        Log,
+
+        /// <summary>
+        /// sinh
+        /// </summary>
+        Sineh,
+        /// <summary>
+        /// cosh
+        /// </summary>
+        Cosineh,
+        /// <summary>
+        /// tanh
+        /// </summary>
+        Tangenth,
+        /// <summary>
+        /// coth
+        /// </summary>
+        Cotangenth,
+        /// <summary>
+        /// sech
+        /// </summary>
+        Secanth,
+        /// <summary>
+        /// csch
+        /// </summary>
+        Cosecanth,
+        /// <summary>
+        /// arsinh
+        /// </summary>
+        Arsineh,
+        /// <summary>
+        /// arcosh
+        /// </summary>
+        Arcosineh,
+        /// <summary>
+        /// artanh
+        /// </summary>
+        Artangenth,
+        /// <summary>
+        /// arcoth
+        /// </summary>
+        Arcotangenth,
+        /// <summary>
+        /// arsech
+        /// </summary>
+        Arsecanth,
+        /// <summary>
+        /// arcsch
+        /// </summary>
+        Arcosecanth,
+
+        /// <summary>
+        /// not
+        /// </summary>
+        Not,
+        /// <summary>
+        /// and
+        /// </summary>
+        And,
+        /// <summary>
+        /// or
+        /// </summary>
+        Or,
+        /// <summary>
+        /// xor
+        /// </summary>
+        XOr,
+
+        /// <summary>
+        /// Exponential
+        /// </summary>
+        Exp,
+        /// <summary>
+        /// deriv
+        /// </summary>
+        Derivative
 
     }
 

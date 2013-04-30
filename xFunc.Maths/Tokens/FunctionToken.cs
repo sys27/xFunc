23a504880eedf9accef0e94e675465d91d63a75b
sys_27 <sys_27@mail.ru>
2013-04-30 16:47:52 +0300

    Implemented tokens...

--- xFunc.Maths/Tokens/FunctionToken.cs
+++ xFunc.Maths/Tokens/FunctionToken.cs
@@ -6,6 +6,21 @@ namespace xFunc.Maths.Tokens
     public class FunctionToken : IToken
     {
 
+        private Functions function;
+
+        public FunctionToken(Functions function)
+        {
+            this.function = function;
+        }
+
+        public Functions Function
+        {
+            get
+            {
+                return function;
+            }
+        }
+
     }
 
 }

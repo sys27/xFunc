a81a87206d1ee1ef7553553f0a423d483afc58f5
sys27 <sys2712@gmail.com>
2013-05-01 14:11:17 +0300

    Added the func collection.

---
+++ xFunc.Maths/Expressions/MathFunctionCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace xFunc.Maths.Expressions
+{
+
+    [Serializable]
+    public class MathFunctionCollection : Dictionary<string, IMathExpression>
+    {
+
+        public MathFunctionCollection()
+        {
+
+        }
+
+        protected MathFunctionCollection(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    }
+
+}

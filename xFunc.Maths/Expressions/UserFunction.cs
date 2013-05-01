20e75d5079753ac511a737c0d7aeb58eb3560a8a
sys_27 <sys_27@mail.ru>
2013-05-01 14:04:55 +0300

    Adding an user func.

---
+++ xFunc.Maths/Expressions/UserFunction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.Maths.Expressions
+{
+
+    public class UserFunction : IMathExpression
+    {
+
+        private string function;
+        private IMathExpression[] args;
+
+        public UserFunction()
+        {
+
+        }
+        
+        public UserFunction(string function, IMathExpression[] args)
+        {
+            this.function = function;
+            this.args = args;
+        }
+
+        public double Calculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Calculate(MathParameterCollection parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMathExpression Differentiate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMathExpression Differentiate(Variable variable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMathExpression Clone()
+        {
+            return new UserFunction(function, args);
+        }
+
+        public IMathExpression Parent
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+            }
+        }
+
+    }
+
+}

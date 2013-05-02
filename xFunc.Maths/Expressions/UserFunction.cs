199a4af2cbb4ed3158941a055e9e3a01f67cc5e6
sys_27 <sys_27@mail.ru>
2013-05-02 10:14:18 +0300

    Fix button

--- xFunc.Maths/Expressions/UserFunction.cs
+++ xFunc.Maths/Expressions/UserFunction.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2012-2013 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +24,17 @@ namespace xFunc.Maths.Expressions
     {
 
         private string function;
-        private IMathExpression[] args;
+        private IMathExpression[] arguments;
 
         public UserFunction()
         {
 
         }
-        
+
         public UserFunction(string function, IMathExpression[] args)
         {
             this.function = function;
-            this.args = args;
+            this.arguments = args;
         }
 
         public double Calculate()
@@ -31,6 +45,11 @@ namespace xFunc.Maths.Expressions
         public double Calculate(MathParameterCollection parameters)
         {
             throw new NotImplementedException();
+        }
+
+        public double Calculate(MathParameterCollection parameters, MathFunctionCollection functions)
+        {
+            throw new NotSupportedException();
         }
 
         public IMathExpression Differentiate()
@@ -45,7 +64,7 @@ namespace xFunc.Maths.Expressions
 
         public IMathExpression Clone()
         {
-            return new UserFunction(function, args);
+            return new UserFunction(function, arguments);
         }
 
         public IMathExpression Parent
@@ -56,6 +75,22 @@ namespace xFunc.Maths.Expressions
             }
             set
             {
+            }
+        }
+
+        public string Function
+        {
+            get
+            {
+                return function;
+            }
+        }
+
+        public IMathExpression[] Arguments
+        {
+            get
+            {
+                return arguments;
             }
         }
 

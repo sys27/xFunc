abbd18cefa7d5d1a3934da405127b27ed6c1bddd
Dmytro Kyshchenko <sys2712@gmail.com>
2013-05-04 23:55:37 +0300

    Added copyright

--- xFunc.Maths/MathParser.cs
+++ xFunc.Maths/MathParser.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using xFunc.Maths.Exceptions;
 using xFunc.Maths.Expressions;
 using xFunc.Maths.Expressions.Bitwise;

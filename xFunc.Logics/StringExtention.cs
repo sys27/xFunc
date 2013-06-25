e60dbb5ba1cd800ed714b40be56ec0b1c6ace41c
sys27 <sys2712@gmail.com>
2013-06-25 09:18:48 +0300

    Added projects for .Net 3.5

---
+++ xFunc.Logics/StringExtention.cs
@@ -0,0 +1,25 @@
+﻿#if NET20 || NET30 || NET35
+
+using System;
+
+namespace xFunc.Logics
+{
+
+    internal static class StringExtention
+    {
+
+        public static bool IsNullOrWhiteSpace(string value)
+        {
+            if (value != null)
+                for (int i = 0; i < value.Length; i++)
+                    if (!char.IsWhiteSpace(value[i]))
+                        return false;
+
+            return true;
+        }
+
+    }
+
+}
+
+#endif

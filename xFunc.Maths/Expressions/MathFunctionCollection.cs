794370614521d863e83ac85bb48af70c6b359d38
sys_27 <sys2712@gmail.com>
2013-06-25 15:32:25 +0300

    #31 Added projects for .Net 3.0

--- xFunc.Maths/Expressions/MathFunctionCollection.cs
+++ xFunc.Maths/Expressions/MathFunctionCollection.cs
@@ -14,9 +14,7 @@
 // limitations under the License.
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace xFunc.Maths.Expressions
 {

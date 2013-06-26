d53736e5fa710330e6738cfbae948681c93087a0
sys27 <sys2712@gmail.com>
2013-06-26 21:11:51 +0300

    Updated built all.bat

--- build all.bat
+++ build all.bat
@@ -1,25 +1,46 @@
 @echo off
 
-msbuild "xFunc (Libs for .Net 2.0).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
+set t=%1
+if "%1"=="" set t=Build
+echo %t% xFunc for .Net 2.0 ...
+msbuild "xFunc (Libs for .Net 2.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed!
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 3.0).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
-if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 3.5).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
-if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 4.0).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
-if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 4.5).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
-if errorlevel 1 goto err
-msbuild "xFunc (Libs for Portable).sln" /nologo /m /p:Configuration=Release
-if errorlevel 0 pause
-if errorlevel 1 goto err
+pause
 
-exit
+echo %t% xFunc for .Net 3.0 ...
+msbuild "xFunc (Libs for .Net 3.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed! 
+if errorlevel 1 goto err
+pause
+
+echo %t% xFunc for .Net 3.5 ...
+msbuild "xFunc (Libs for .Net 3.5).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed! 
+if errorlevel 1 goto err
+pause
+
+echo %t% xFunc for .Net 4.0 ...
+msbuild "xFunc (Libs for .Net 4.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed!
+if errorlevel 1 goto err
+pause
+
+echo %t% xFunc for .Net 4.5 ...
+msbuild "xFunc (Libs for .Net 4.5).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed! 
+if errorlevel 1 goto err
+pause
+
+echo %t% xFunc for Portable ...
+msbuild "xFunc (Libs for Portable).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+if errorlevel 0 echo %t% completed! 
+if errorlevel 1 goto err
+pause
+
+goto end
 
 :err
 pause
+
+:end

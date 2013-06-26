c41d32c33cafe42dca5743718d54363540a44d5f
Dmytro Kyshchenko <sys2712@gmail.com>
2013-06-26 09:56:46 +0300

    .

--- build all.bat
+++ build all.bat
@@ -1,21 +1,21 @@
 @echo off
 
-msbuild "xFunc (Libs for .Net 2.0).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 2.0).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 3.0).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 3.0).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 3.5).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 3.5).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 4.0).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 4.0).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for .Net 4.5).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 4.5).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
-msbuild "xFunc (Libs for Portable).sln" /p:Configuration=Release
+msbuild "xFunc (Libs for Portable).sln" /nologo /m /p:Configuration=Release
 if errorlevel 0 pause
 if errorlevel 1 goto err
 

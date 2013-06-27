ba7add28461d42299a0730639eaed3074061eef5
sys_27 <sys_27@mail.ru>
2013-06-27 13:48:31 +0300

    Added the def function.

--- xFunc.Test/Expressions/Maths/DefineTest.cs
+++ xFunc.Test/Expressions/Maths/DefineTest.cs
@@ -8,13 +8,13 @@ namespace xFunc.Test.Expressions.Maths
 {
 
     [TestClass]
-    public class AssignTest
+    public class DefineTest
     {
 
         [TestMethod]
         public void CalculateTest1()
         {
-            IMathExpression exp = new Assign(new Variable("x"), new Number(1));
+            IMathExpression exp = new Define(new Variable("x"), new Number(1));
             MathParameterCollection parameters = new MathParameterCollection();
 
             double answer = exp.Calculate(parameters);
@@ -26,7 +26,7 @@ namespace xFunc.Test.Expressions.Maths
         [TestMethod]
         public void CalculateTest2()
         {
-            IMathExpression exp = new Assign(new Variable("x"), new Sin(new Number(1)) { AngleMeasurement = AngleMeasurement.Radian });
+            IMathExpression exp = new Define(new Variable("x"), new Sin(new Number(1)) { AngleMeasurement = AngleMeasurement.Radian });
             MathParameterCollection parameters = new MathParameterCollection();
 
             double answer = exp.Calculate(parameters);
@@ -38,7 +38,7 @@ namespace xFunc.Test.Expressions.Maths
         [TestMethod]
         public void CalculateTest3()
         {
-            IMathExpression exp = new Assign(new Variable("x"), new Mul(new Number(4), new Add(new Number(8), new Number(1))));
+            IMathExpression exp = new Define(new Variable("x"), new Mul(new Number(4), new Add(new Number(8), new Number(1))));
             MathParameterCollection parameters = new MathParameterCollection();
 
             double answer = exp.Calculate(parameters);

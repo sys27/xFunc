1d36bde5eaf4980d40349309e38f73d93406b724
sys_27 <sys_27@mail.ru>
2013-06-30 12:03:58 +0300

    Fixed #35

--- xFunc/Properties/AssemblyInfo.cs
+++ xFunc/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.0.1")]
+[assembly: AssemblyFileVersion("2.3.0.1")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

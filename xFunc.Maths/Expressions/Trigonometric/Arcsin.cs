e1aac1042e52637fbd4e6ebb49cfef4a150fa6f8
sys27 <sys2712@gmail.com>
2013-07-21 13:49:22 +0300

    #21

--- xFunc.Maths/Expressions/Trigonometric/Arcsin.cs
+++ xFunc.Maths/Expressions/Trigonometric/Arcsin.cs
@@ -17,6 +17,9 @@ using System;
 namespace xFunc.Maths.Expressions.Trigonometric
 {
 
+    /// <summary>
+    /// Represents teh Arcsine function.
+    /// </summary>
     public class Arcsin : TrigonometryMathExpression
     {
 

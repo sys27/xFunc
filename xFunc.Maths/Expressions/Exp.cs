f8b31399138a9e57826bd7bc1a36bba13425aec8
Dmytro Kyshchenko <sys2712@gmail.com>
2013-08-16 22:31:34 +0300

    #21

--- xFunc.Maths/Expressions/Exp.cs
+++ xFunc.Maths/Expressions/Exp.cs
@@ -43,16 +43,40 @@ namespace xFunc.Maths.Expressions
             return ToString("exp({0})");
         }
 
+        /// <summary>
+        /// Calculates this mathemarical expression. Don't use this method if your expression has variables or functions.
+        /// </summary>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
         public override double Calculate()
         {
             return Math.Exp(argument.Calculate());
         }
 
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">A collection of variables that are used in the expression.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="MathParameterCollection" />
         public override double Calculate(MathParameterCollection parameters)
         {
             return Math.Exp(argument.Calculate(parameters));
         }
 
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">A collection of variables that are used in the expression.</param>
+        /// <param name="functions">A collection of functions that are used in the expression.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="MathParameterCollection" />
+        /// <seealso cref="MathFunctionCollection" />
         public override double Calculate(MathParameterCollection parameters, MathFunctionCollection functions)
         {
             return Math.Exp(argument.Calculate(parameters, functions));

4d2a993b6fd8b8f87416cd30c74c579ddda2e91f
Dmytro Kyshchenko <sys2712@gmail.com>
2013-08-26 15:56:53 +0300

    Added SharedAssemblyInfo.cs

--- xFunc.Logics/Properties/AssemblyInfo.cs
+++ xFunc.Logics/Properties/AssemblyInfo.cs
@@ -1,28 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 [assembly: AssemblyTitle("xFunc.Logics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xFunc.Logics")]
-[assembly: AssemblyCopyright("Copyright © 2012-2013 Dmitry Kischenko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
 
 #if !PORTABLE
 [assembly: ComVisible(false)]
 [assembly: Guid("817c2e5c-5a4a-493b-89ce-57d62f3dd7c5")]
 #endif
-
-[assembly: AssemblyVersion("2.4.1")]
-[assembly: AssemblyFileVersion("2.4.1")]
-[assembly: AssemblyInformationalVersion("2.4.1")]
-[assembly: NeutralResourcesLanguageAttribute("en")]

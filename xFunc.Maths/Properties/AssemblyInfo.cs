9717419de1d2d0fb12b143add3e1edc286895983
sys_27 <sys_27@mail.ru>
2013-08-26 15:56:53 +0300

    Added SharedAssemblyInfo.cs

--- xFunc.Maths/Properties/AssemblyInfo.cs
+++ xFunc.Maths/Properties/AssemblyInfo.cs
@@ -1,28 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 [assembly: AssemblyTitle("xFunc.Maths")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xFunc.Maths")]
-[assembly: AssemblyCopyright("Copyright © 2012-2013 Dmitry Kischenko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
 
 #if !PORTABLE
 [assembly: ComVisible(false)]
 [assembly: Guid("cab94f81-2f6d-40bd-8a94-272845642538")]
 #endif
-
-[assembly: AssemblyVersion("2.4.1")]
-[assembly: AssemblyFileVersion("2.4.1")]
-[assembly: AssemblyInformationalVersion("2.4.1")]
-[assembly: NeutralResourcesLanguageAttribute("en")]

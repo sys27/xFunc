0ced59cbe69e76c6f0eb2177b31eb73f86130506
sys27 <sys2712@gmail.com>
2013-09-01 22:32:50 +0300

    New API

---
+++ xFunc.Maths/Results/NumberResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace xFunc.Maths.Results
+{
+
+    public class NumberResult : IResult
+    {
+
+        private double number;
+
+        public NumberResult(double number)
+        {
+            this.number = number;
+        }
+
+        public override string ToString()
+        {
+            return number.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public double Result
+        {
+            get
+            {
+                return number;
+            }
+        }
+
+    }
+
+}

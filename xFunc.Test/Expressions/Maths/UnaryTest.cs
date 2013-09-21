61520e24f6a83e5fc6598a8fc53597c4f160cc9c
sys_27 <sys2712@gmail.com>
2013-09-21 13:16:51 +0300

    Fixed #44

---
+++ xFunc.Test/Expressions/Maths/UnaryTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions.Trigonometric;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class UnaryTest
+    {
+
+        [TestMethod]
+        public void EqualsTest1()
+        {
+            var sine1 = new Sin(new Number(2));
+            var sine2 = new Sin(new Number(2));
+
+            Assert.AreEqual(sine1, sine2);
+        }
+
+        [TestMethod]
+        public void EqualsTest2()
+        {
+            var sine = new Sin(new Number(2));
+            var ln = new Ln(new Number(2));
+
+            Assert.AreNotEqual(sine, ln);
+        }
+
+    }
+
+}

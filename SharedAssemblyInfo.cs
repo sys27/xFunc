11420e321b7942c5144160f0647adfdd1b096eed
sys27 <sys2712@gmail.com>
2013-09-22 12:22:13 +0300

    v2.5.8

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.5.7")]
-[assembly: AssemblyFileVersion("2.5.7")]
-[assembly: AssemblyInformationalVersion("2.5.7")]
+[assembly: AssemblyVersion("2.5.8")]
+[assembly: AssemblyFileVersion("2.5.8")]
+[assembly: AssemblyInformationalVersion("2.5.8")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

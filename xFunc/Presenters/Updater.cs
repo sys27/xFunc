9b8b4942de4b4b85f36e20dd255a41caaf988033
Dmytro Kyshchenko <sys2712@gmail.com>
2013-09-30 18:05:50 +0300

    Closed #37

---
+++ xFunc/Presenters/Updater.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Security;
+
+namespace xFunc.Presenters
+{
+
+    /// <summary>
+    /// Checks updates of this program.
+    /// </summary>
+    internal class Updater
+    {
+
+        private const string checkUri = "http://xfunc.codeplex.com/releases/";
+        private const int currentRelease = 012727;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Updater"/> class.
+        /// </summary>
+        internal Updater()
+        {
+        }
+
+        /// <summary>
+        /// Checks the updates.
+        /// </summary>
+        /// <returns>The url to download a new release.</returns>
+        internal string CheckUpdates()
+        {
+            string url = null;
+
+            try
+            {
+                var request = WebRequest.Create(checkUri);
+                var response = request.GetResponse();
+                var responseUri = response.ResponseUri.ToString();
+
+                var releaseNumber = responseUri.Substring(responseUri.LastIndexOf('/') + 1);
+                var release = int.Parse(releaseNumber);
+
+                if (release > currentRelease)
+                    url = responseUri;
+            }
+            catch (SecurityException)
+            {
+            }
+            catch (FormatException)
+            {
+            }
+
+            return url;
+        }
+
+    }
+
+}

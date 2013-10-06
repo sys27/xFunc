2c0b9c0cfaf91e7c5e6f2c0affaf18731944ac37
Dmytro Kyshchenko <sys2712@gmail.com>
2013-10-06 11:34:57 +0300

    .

--- xFunc.Maths/Expressions/GCD.cs
+++ xFunc.Maths/Expressions/GCD.cs
@@ -13,7 +13,9 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 using System;
+#if NET35_OR_GREATER || PORTABLE
 using System.Linq;
+#endif
 
 namespace xFunc.Maths.Expressions
 {

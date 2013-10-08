14aa6974dc701748b3c4f662ef5874c651fc59c7
sys_27 <sys2712@gmail.com>
2013-10-08 13:37:42 +0300

    Fixed memory leak.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.7.1")]
-[assembly: AssemblyFileVersion("2.7.1")]
-[assembly: AssemblyInformationalVersion("2.7.1")]
+[assembly: AssemblyVersion("2.7.2")]
+[assembly: AssemblyFileVersion("2.7.2")]
+[assembly: AssemblyInformationalVersion("2.7.2")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

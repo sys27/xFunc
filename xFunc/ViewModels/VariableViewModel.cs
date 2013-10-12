5692983b95bac2349318edb37d634d0f9b006028
Dmytro Kyshchenko <sys2712@gmail.com>
2013-10-12 16:08:15 +0300

    variables

---
+++ xFunc/ViewModels/VariableViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace xFunc.ViewModels
+{
+
+    public class VariableViewModel
+    {
+
+        private string variable;
+        private string value;
+        private bool isReadOnly;
+
+        public VariableViewModel(string variable, string value, bool isReadOnly)
+        {
+            this.variable = variable;
+            this.value = value;
+            this.isReadOnly = isReadOnly;
+        }
+
+        public string Variable
+        {
+            get
+            {
+                return variable;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return isReadOnly;
+            }
+        }
+
+    }
+
+}

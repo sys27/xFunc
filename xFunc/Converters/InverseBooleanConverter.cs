76a461b18b2885faf89d33577875a04e46e4ce90
sys27 <sys2712@gmail.com>
2013-10-19 11:59:28 +0300

    #52

---
+++ xFunc/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace xFunc.Converters
+{
+
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(bool))
+                return !(bool)value;
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(bool))
+                return !(bool)value;
+
+            return null;
+        }
+
+    }
+
+}

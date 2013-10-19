06ab12455a603743dbd1f7f76cefbbfbab85ca2b
sys27 <sys2712@gmail.com>
2013-10-19 18:04:50 +0300

    #52

--- xFunc/Presenters/Updater.cs
+++ xFunc/Presenters/Updater.cs
@@ -26,7 +26,7 @@ namespace xFunc.Presenters
     {
 
         private const string checkUri = "http://xfunc.codeplex.com/releases/";
-        private const int currentRelease = 113205;
+        private const int currentRelease = 113669;
         private string updateUri;
         private bool hasUpdates;
 

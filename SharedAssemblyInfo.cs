b809f599aef1729e44f2be5ce1e11a39eb56e025
sys_27 <sys_27@mail.ru>
2013-10-25 17:15:46 +0300

    Closed #57

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.8.2")]
-[assembly: AssemblyFileVersion("2.8.2")]
-[assembly: AssemblyInformationalVersion("2.8.2")]
+[assembly: AssemblyVersion("2.8.3")]
+[assembly: AssemblyFileVersion("2.8.3")]
+[assembly: AssemblyInformationalVersion("2.8.3")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
--- xFunc/Presenters/Updater.cs
+++ xFunc/Presenters/Updater.cs
@@ -26,7 +26,7 @@ namespace xFunc.Presenters
     {
 
         private const string checkUri = "http://xfunc.codeplex.com/releases/";
-        private const int currentRelease = 113720;
+        private const int currentRelease = 113947;
         private string updateUri;
         private bool hasUpdates;
 

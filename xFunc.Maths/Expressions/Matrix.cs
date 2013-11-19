0847bbc84c48d129d4134fd2bc5cd19ce28c73a8
sys_27 <sys_27@mail.ru>
2013-11-19 08:25:19 +0200

    Added Matrix.cs

---
+++ xFunc.Maths/Expressions/Matrix.cs
@@ -0,0 +1,164 @@
+﻿// Copyright 2012-2013 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Text;
+
+namespace xFunc.Maths.Expressions
+{
+
+    public class Matrix : DifferentParametersExpression
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Matrix"/> class.
+        /// </summary>
+        internal Matrix()
+            : base(null, -1)
+        {
+        }
+
+        public Matrix(IMathExpression[] args, int countOfParams)
+            : base(args, countOfParams)
+        {
+            if (args == null)
+                throw new ArgumentNullException("args");
+            if (args.Length < 2 && args.Length != countOfParams)
+                throw new ArgumentException();
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode(3121, 8369);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append('{');
+            foreach (var item in arguments)
+                sb.Append(item).Append(", ");
+            sb.Remove(sb.Length - 2, 2).Append('}');
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression. Don't use this method if your expression has variables or user-functions.
+        /// </summary>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public override double Calculate()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public override double Calculate(ExpressionParameters parameters)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <returns>
+        /// Returns a derivative of the expression.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public override IMathExpression Differentiate()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <param name="variable">The variable of differentiation.</param>
+        /// <returns>
+        /// Returns a derivative of the expression of several variables.
+        /// </returns>
+        /// <seealso cref="Variable" />
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public override IMathExpression Differentiate(Variable variable)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Clones this instance of the <see cref="IMathExpression" />.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="IMathExpression" /> that is a clone of this instance.
+        /// </returns>
+        public override IMathExpression Clone()
+        {
+            return new Matrix(CloneArguments(), countOfParams);
+        }
+
+        /// <summary>
+        /// Gets the minimum count of parameters.
+        /// </summary>
+        /// <value>
+        /// The minimum count of parameters.
+        /// </value>
+        public override int MinCountOfParams
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the maximum count of parameters. -1 - Infinity.
+        /// </summary>
+        /// <value>
+        /// The maximum count of parameters.
+        /// </value>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override int MaxCountOfParams
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+    }
+
+}

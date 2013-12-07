543c643ece7bf98f243ca05007bccf5c1b355221
sys_27 <sys2712@gmail.com>
2013-12-07 17:04:27 +0200

    sub vectors

--- xFunc.Test/Expressions/Maths/VectorTest.cs
+++ xFunc.Test/Expressions/Maths/VectorTest.cs
@@ -41,8 +41,29 @@ namespace xFunc.Test.Expressions.Maths
             var vector1 = new Vector(new[] { new Number(2), new Number(3) });
             var vector2 = new Vector(new[] { new Number(7), new Number(1), new Number(3) });
 
-            var expected = new Vector(new[] { new Number(9), new Number(4) });
             var result = vector1.Add(vector2);
+        }
+
+        [TestMethod]
+        public void SubVectorsTest()
+        {
+            var vector1 = new Vector(new[] { new Number(2), new Number(3) });
+            var vector2 = new Vector(new[] { new Number(7), new Number(1) });
+
+            var expected = new Vector(new[] { new Number(-5), new Number(2) });
+            var result = vector1.Sub(vector2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VectorIsInvalidException))]
+        public void SubVectorsDiffSizeTest()
+        {
+            var vector1 = new Vector(new[] { new Number(2), new Number(3) });
+            var vector2 = new Vector(new[] { new Number(7), new Number(1), new Number(3) });
+
+            var result = vector1.Sub(vector2);
         }
 
     }

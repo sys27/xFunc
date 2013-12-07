27be5ba295955a7498fe45ae99932ea6420ed9ab
sys27 <sys2712@gmail.com>
2013-12-07 16:58:48 +0200

    .

--- xFunc.Maths/Expressions/Matrices/Matrix.cs
+++ xFunc.Maths/Expressions/Matrices/Matrix.cs
@@ -157,6 +157,7 @@ namespace xFunc.Maths.Expressions.Matrices
             var size = args[0].CountOfParams;
 
             if (args.Any(exp => !(exp is Vector) || exp.CountOfParams != size))
+                // todo: message
                 throw new MatrixIsInvalidException();
         }
 

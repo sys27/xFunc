dd51d6d18dcfee5148941f0946f62d72a97de15a
sys_27 <sys_27@mail.ru>
2013-12-07 17:19:46 +0200

    add/sub matrices

--- xFunc.Test/Expressions/Maths/MatrixTest.cs
+++ xFunc.Test/Expressions/Maths/MatrixTest.cs
@@ -15,17 +15,101 @@ namespace xFunc.Test.Expressions.Maths
         {
             var vector1 = new Vector(new[] { new Number(2), new Number(3) });
             var vector2 = new Vector(new[] { new Number(9), new Number(5) });
-            var matrix = new Matrix(new[] { vector1, vector2 }, 2);
+            var matrix = new Matrix(new[] { vector1, vector2 });
             var number = new Number(5);
 
             var expected = new Matrix(new[] 
             { 
                 new Vector(new[] { new Number(10), new Number(15) }),
                 new Vector(new[] { new Number(45), new Number(25) })
-            }, 2);
+            });
             var result = matrix.Mul(number);
 
             Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddMatricesTest()
+        {
+            var matrix1 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(6), new Number(3) }), 
+                new Vector(new[] { new Number(2), new Number(1) }) 
+            });
+            var matrix2 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(9), new Number(2) }), 
+                new Vector(new[] { new Number(4), new Number(3) }) 
+            });
+
+            var expected = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(15), new Number(5) }), 
+                new Vector(new[] { new Number(6), new Number(4) }) 
+            });
+            var result = matrix1.Add(matrix2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MatrixIsInvalidException))]
+        public void AddMatricesDiffSizeTest()
+        {
+            var matrix1 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(6), new Number(3) }), 
+                new Vector(new[] { new Number(2), new Number(1) }) 
+            });
+            var matrix2 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(9), new Number(2) }), 
+                new Vector(new[] { new Number(4), new Number(3), new Number(9) }) 
+            });
+
+            var result = matrix1.Add(matrix2);
+        }
+
+        [TestMethod]
+        public void SubMatricesTest()
+        {
+            var matrix1 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(6), new Number(3) }), 
+                new Vector(new[] { new Number(2), new Number(1) }) 
+            });
+            var matrix2 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(9), new Number(2) }), 
+                new Vector(new[] { new Number(4), new Number(3) }) 
+            });
+
+            var expected = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(-3), new Number(1) }), 
+                new Vector(new[] { new Number(-2), new Number(-2) }) 
+            });
+            var result = matrix1.Sub(matrix2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MatrixIsInvalidException))]
+        public void SubMatricesDiffSizeTest()
+        {
+            var matrix1 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(6), new Number(3) }), 
+                new Vector(new[] { new Number(2), new Number(1) }) 
+            });
+            var matrix2 = new Matrix(new[] 
+            { 
+                new Vector(new[] { new Number(9), new Number(2) }), 
+                new Vector(new[] { new Number(4), new Number(3), new Number(9) }) 
+            });
+
+            var result = matrix1.Sub(matrix2);
         }
 
     }

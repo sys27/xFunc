febd6a95aa05d5ee55a86fd14ffc739d75d5a65e
sys27 <sys2712@gmail.com>
2014-01-08 11:11:00 +0200

    .

--- xFunc.Maths/Expressions/Trigonometric/TrigonometricExpression.cs
+++ xFunc.Maths/Expressions/Trigonometric/TrigonometricExpression.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2012-2013 Dmitry Kischenko
+﻿// Copyright 2012-2014 Dmitry Kischenko
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License.

65f4bfa70062080b1ec1aede7c8c35b269f88b08
sys_27 <sys2712@gmail.com>
2014-02-20 09:53:46 +0200

    Added tests.

---
+++ xFunc.Test/Expressions/Maths/DelegateExpressionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions;
+using xFunc.Maths.Expressions.Collections;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class DelegateExpressionTest
+    {
+
+        [TestMethod]
+        public void CalculateTest1()
+        {
+            var parameters = new ParameterCollection()
+            {
+                new Parameter("x", 10)
+            };
+            var func = new DelegateExpression(p => p.Parameters["x"] + 1);
+
+            var result = func.Calculate(parameters);
+
+            Assert.AreEqual(11.0, result);
+        }
+
+        [TestMethod]
+        public void CalculateTest2()
+        {
+            var func = new DelegateExpression(p => 10.0);
+
+            var result = func.Calculate(null);
+
+            Assert.AreEqual(10.0, result);
+        }
+
+    }
+
+}

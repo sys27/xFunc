45a05eb7392e820aa54ad411b29d8367436e86e4
sys27 <sys2712@gmail.com>
2014-02-20 09:43:17 +0200

    Created DelegateExpression.cs

---
+++ xFunc.Maths/Expressions/DelegateExpression.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2012-2014 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+
+namespace xFunc.Maths.Expressions
+{
+
+    public class DelegateExpression : IExpression
+    {
+
+        /// <summary>
+        /// The parent expression of this expression.
+        /// </summary>
+        protected IExpression parent;
+        private Func<ExpressionParameters, object> func;
+
+        internal DelegateExpression() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateExpression"/> class.
+        /// </summary>
+        /// <param name="func">The delegate of function.</param>
+        public DelegateExpression(Func<ExpressionParameters, object> func)
+        {
+            this.func = func;
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression. Don't use this method if your expression has variables or user-functions.
+        /// </summary>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        public object Calculate()
+        {
+            return func(null);
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        public object Calculate(ExpressionParameters parameters)
+        {
+            return func(parameters);
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <returns>
+        /// Returns a derivative of the expression.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public IExpression Differentiate()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <param name="variable">The variable of differentiation.</param>
+        /// <returns>
+        /// Returns a derivative of the expression of several variables.
+        /// </returns>
+        /// <seealso cref="Variable" />
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        public IExpression Differentiate(Variable variable)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Clones this instance of the <see cref="IExpression" />.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="IExpression" /> that is a clone of this instance.
+        /// </returns>
+        public IExpression Clone()
+        {
+            // todo: ...
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get or Set the parent expression.
+        /// </summary>
+        public IExpression Parent
+        {
+            get
+            {
+                return parent;
+            }
+            set
+            {
+                parent = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the minimum count of parameters.
+        /// </summary>
+        /// <value>
+        /// The minimum count of parameters.
+        /// </value>
+        public int MinCountOfParams
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the maximum count of parameters. -1 - Infinity.
+        /// </summary>
+        /// <value>
+        /// The maximum count of parameters.
+        /// </value>
+        public int MaxCountOfParams
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the count of parameters.
+        /// </summary>
+        /// <value>
+        /// The count of parameters.
+        /// </value>
+        public int CountOfParams
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+    }
+
+}

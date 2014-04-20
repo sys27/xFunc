f31dcc195b1354314db808cccaabc268a7790327
Dmytro Kyshchenko <sys2712@gmail.com>
2014-04-20 18:46:42 +0300

    Fixed project files.

---
+++ xFunc.Maths/Func.cs
@@ -0,0 +1,9 @@
+#if NET20 || NET30
+namespace System
+{
+
+	public delegate TResult Func<in T, out TResult>(T arg);
+    public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+
+}
+#endif

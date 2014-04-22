ebdf16db1e554c659a505eb689ed0c97de40d7bb
sys27 <sys2712@gmail.com>
2014-04-22 22:35:51 +0300

    Added buttons.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.12.0")]
-[assembly: AssemblyFileVersion("2.12.0")]
-[assembly: AssemblyInformationalVersion("2.12.0")]
+[assembly: AssemblyVersion("2.12.1")]
+[assembly: AssemblyFileVersion("2.12.1")]
+[assembly: AssemblyInformationalVersion("2.12.1")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

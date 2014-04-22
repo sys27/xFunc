1dc6ccfd4ace172fd785024d1d448be5e2d43c5d
sys27 <sys2712@gmail.com>
2014-04-22 22:17:39 +0300

    Added tests for 'ceil' and 'floor'.

---
+++ xFunc.Test/Expressions/Maths/CeilTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Test.Expressions.Maths
+{
+
+    [TestClass]
+    public class CeilTest
+    {
+
+        [TestMethod]
+        public void CeilCalculate()
+        {
+            var ceil = new Ceil(new Number(5.55555555));
+            var result = ceil.Calculate();
+            var expected = 6.0;
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+
+}

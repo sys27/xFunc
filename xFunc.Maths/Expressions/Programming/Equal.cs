e2f0daf3fbad63e1c52e03057e412f5e81c7712c
sys_27 <sys2712@gmail.com>
2014-04-26 22:14:14 +0300

    .

---
+++ xFunc.Maths/Expressions/Programming/Equal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.Maths.Expressions.Programming
+{
+
+    public class Equal : BinaryExpression
+    {
+
+        internal Equal() { }
+
+        public Equal(IExpression left, IExpression right)
+            : base(left, right) { }
+
+        public override object Calculate(ExpressionParameters parameters)
+        {
+            // todo: refactor...
+            var leftValue = (double)left.Calculate(parameters) == 0 ? false : true;
+            var rightValue = (double)right.Calculate(parameters) == 0 ? false : true;
+
+            return leftValue == rightValue;
+        }
+
+        public override IExpression Clone()
+        {
+            return new Equal(left.Clone(), right.Clone());
+        }
+
+        public override IExpression Differentiate(Variable variable)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+
+}

6cc299ce9d480fd7ee0b661f9c39d305e5b5a3f7
sys27 <sys2712@gmail.com>
2014-04-27 18:00:26 +0300

    Added tests <, <=, >, >=

---
+++ xFunc.Test/Expressions/Maths/Programming/GreaterOrEqualTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions;
+using xFunc.Maths.Expressions.Collections;
+using xFunc.Maths.Expressions.Programming;
+
+namespace xFunc.Test.Expressions.Maths.Programming
+{
+
+    [TestClass]
+    public class GreaterOrEqualTest
+    {
+
+        [TestMethod]
+        public void CalculateGreaterTrueTest1()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 463) };
+            var lessThen = new GreaterOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(1, lessThen.Calculate(parameters));
+        }
+
+        [TestMethod]
+        public void CalculateGreaterTrueTest2()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 10) };
+            var lessThen = new GreaterOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(1, lessThen.Calculate(parameters));
+        }
+
+        [TestMethod]
+        public void CalculateGreaterFalseTest()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 0) };
+            var lessThen = new GreaterOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(0, lessThen.Calculate(parameters));
+        }
+
+    }
+
+}

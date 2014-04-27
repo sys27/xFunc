537fb54b37117ba6d6722bc76ef573d6656092f6
Dmytro Kyshchenko <sys2712@gmail.com>
2014-04-27 18:00:26 +0300

    Added tests <, <=, >, >=

---
+++ xFunc.Test/Expressions/Maths/Programming/LessOrEqualTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions;
+using xFunc.Maths.Expressions.Collections;
+using xFunc.Maths.Expressions.Programming;
+
+namespace xFunc.Test.Expressions.Maths.Programming
+{
+
+    [TestClass]
+    public class LessOrEqualTest
+    {
+
+        [TestMethod]
+        public void CalculateLessTrueTest1()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 0) };
+            var lessThen = new LessOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(1, lessThen.Calculate(parameters));
+        }
+
+        [TestMethod]
+        public void CalculateLessTrueTest2()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 10) };
+            var lessThen = new LessOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(1, lessThen.Calculate(parameters));
+        }
+
+        [TestMethod]
+        public void CalculateLessFalseTest()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 666) };
+            var lessThen = new LessOrEqual(new Variable("x"), new Number(10));
+
+            Assert.AreEqual(0, lessThen.Calculate(parameters));
+        }
+
+    }
+
+}

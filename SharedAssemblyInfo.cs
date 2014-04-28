70cef97acd27e55fe38562591957ed86d2096e0a
sys_27 <sys2712@gmail.com>
2014-04-28 18:12:00 +0300

    .

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.12.3")]
-[assembly: AssemblyFileVersion("2.12.3")]
-[assembly: AssemblyInformationalVersion("2.12.3")]
+[assembly: AssemblyVersion("2.13.0")]
+[assembly: AssemblyFileVersion("2.13.0")]
+[assembly: AssemblyInformationalVersion("2.13.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

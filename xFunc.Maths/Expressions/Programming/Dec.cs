b2796f4578c462e97aa76ef3f4776b2f1df971e9
Dmytro Kyshchenko <sys2712@gmail.com>
2014-04-29 11:01:27 +0300

    #70 Added inc and dec expressions.

---
+++ xFunc.Maths/Expressions/Programming/Dec.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.Maths.Expressions.Programming
+{
+
+    /// <summary>
+    /// Represents the decrement operator.
+    /// </summary>
+    public class Dec : UnaryExpression
+    {
+
+        internal Dec() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dec"/> class.
+        /// </summary>
+        /// <param name="argument">The expression.</param>
+        public Dec(IExpression argument)
+            : base(argument) { }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return ToString("{0}--");
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        public override object Calculate(ExpressionParameters parameters)
+        {
+            var var = (Variable)argument;
+            var newValue = (double)var.Calculate(parameters) - 1;
+            parameters.Parameters[var.Name] = newValue;
+
+            return newValue;
+        }
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="Inc" /> that is a clone of this instance.
+        /// </returns>
+        public override IExpression Clone()
+        {
+            return new Dec(argument.Clone());
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <param name="variable">The variable of differentiation.</param>
+        /// <returns>
+        /// Returns a derivative of the expression of several variables.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        /// <seealso cref="Variable" />
+        protected override IExpression _Differentiation(Variable variable)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Gets or sets the expression.
+        /// </summary>
+        /// <value>The expression.</value>
+        public virtual IExpression Argument
+        {
+            get
+            {
+                return argument;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (!(value is Variable))
+                        throw new NotSupportedException();
+
+                    value.Parent = this;
+                }
+
+                argument = value;
+            }
+        }
+
+    }
+}
---
+++ xFunc.Maths/Expressions/Programming/Inc.cs
@@ -0,0 +1,114 @@
+﻿// Copyright 2012-2014 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+
+namespace xFunc.Maths.Expressions.Programming
+{
+
+    /// <summary>
+    /// Represents the increment operator.
+    /// </summary>
+    public class Inc : UnaryExpression
+    {
+
+        internal Inc() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Inc"/> class.
+        /// </summary>
+        /// <param name="argument">The expression.</param>
+        public Inc(IExpression argument)
+            : base(argument) { }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return ToString("{0}++");
+        }
+
+        /// <summary>
+        /// Calculates this mathemarical expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        public override object Calculate(ExpressionParameters parameters)
+        {
+            var var = (Variable)argument;
+            var newValue = (double)var.Calculate(parameters) + 1;
+            parameters.Parameters[var.Name] = newValue;
+
+            return newValue;
+        }
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="Inc" /> that is a clone of this instance.
+        /// </returns>
+        public override IExpression Clone()
+        {
+            return new Inc(argument.Clone());
+        }
+
+        /// <summary>
+        /// Calculates a derivative of the expression.
+        /// </summary>
+        /// <param name="variable">The variable of differentiation.</param>
+        /// <returns>
+        /// Returns a derivative of the expression of several variables.
+        /// </returns>
+        /// <exception cref="System.NotSupportedException">Always.</exception>
+        /// <seealso cref="Variable" />
+        protected override IExpression _Differentiation(Variable variable)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Gets or sets the expression.
+        /// </summary>
+        /// <value>The expression.</value>
+        public virtual IExpression Argument
+        {
+            get
+            {
+                return argument;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (!(value is Variable))
+                        throw new NotSupportedException();
+
+                    value.Parent = this;
+                }
+
+                argument = value;
+            }
+        }
+
+    }
+
+}

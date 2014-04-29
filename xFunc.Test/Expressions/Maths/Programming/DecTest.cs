1a345c6a5ee03e08d8a5f553744e1314633d120d
sys27 <sys2712@gmail.com>
2014-04-29 11:01:27 +0300

    #70 Added inc and dec expressions.

---
+++ xFunc.Test/Expressions/Maths/Programming/DecTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.Maths.Expressions;
+using xFunc.Maths.Expressions.Collections;
+using xFunc.Maths.Expressions.Programming;
+
+namespace xFunc.Test.Expressions.Maths.Programming
+{
+
+    [TestClass]
+    public class DecTest
+    {
+
+        [TestMethod]
+        public void DecCalcTest()
+        {
+            var parameters = new ParameterCollection() { new Parameter("x", 10) };
+            var dec = new Dec(new Variable("x"));
+            var result = (double)dec.Calculate(parameters);
+
+            Assert.AreEqual(9, result);
+            Assert.AreEqual(9, parameters["x"]);
+        }
+
+    }
+
+}

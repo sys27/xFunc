34c5f7be79979b09bd2da4a384cc130c92b89e4f
Dmytro Kyshchenko <sys2712@gmail.com>
2014-04-29 16:35:38 +0300

    #70 Fixed bugs.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.13.1")]
-[assembly: AssemblyFileVersion("2.13.1")]
-[assembly: AssemblyInformationalVersion("2.13.1")]
+[assembly: AssemblyVersion("2.13.2")]
+[assembly: AssemblyFileVersion("2.13.2")]
+[assembly: AssemblyInformationalVersion("2.13.2")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

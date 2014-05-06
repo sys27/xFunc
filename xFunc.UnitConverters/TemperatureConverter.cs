4c9129d57e3bb92368f04f4b4017e10609610011
sys27 <sys2712@gmail.com>
2014-05-06 19:55:48 +0300

    #24 Added a temperature converter.

---
+++ xFunc.UnitConverters/TemperatureConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public class TemperatureConverter : Converter<TemperatureUnits>
+    {
+
+        static TemperatureConverter()
+        {
+            BaseUnit = TemperatureUnits.Celsius;
+            RegisterConversion(TemperatureUnits.Fahrenheit, t => t * (9.0 / 5) + 32, t => (t - 32) * 5.0 / 9);
+            RegisterConversion(TemperatureUnits.Kelvin, t => t + 273.15, t => t - 273.15);
+            RegisterConversion(TemperatureUnits.Rankine, t => (t + 273.15) * (9.0 / 5), t => (t - 491.67) * (5.0 / 9));
+            RegisterConversion(TemperatureUnits.Delisle, t => (100 - t) * (3.0 / 2), t => 100 - t * (2.0 / 3));
+            RegisterConversion(TemperatureUnits.Newton, t => t * (0.33), t => t * (100.0 / 33));
+            RegisterConversion(TemperatureUnits.Réaumur, t => t * (4.0 / 5), t => t * (5.0 / 4));
+            RegisterConversion(TemperatureUnits.Rømer, t => t * (21.0 / 40) + 7.5, t => (t - 7.5) * (40.0 / 21));
+        }
+
+    }
+
+}
---
+++ xFunc.UnitConverters/TemperatureUnits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public enum TemperatureUnits
+    {
+
+        Celsius,
+        Fahrenheit,
+        Kelvin,
+        Rankine,
+        Delisle,
+        Newton,
+        Réaumur,
+        Rømer
+
+    }
+
+}

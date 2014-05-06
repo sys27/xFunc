ed15ffb37dbecbfc9ccd073c02b92f3a3d05219e
sys27 <sys2712@gmail.com>
2014-05-06 22:06:00 +0300

    .

--- xFunc.Test/Converters/TimeTest.cs
+++ xFunc.Test/Converters/TimeTest.cs
@@ -59,6 +59,54 @@ namespace xFunc.Test.Converters
             Assert.AreEqual(42, value);
         }
 
+        [TestMethod]
+        public void FromSToH()
+        {
+            var value = conv.Convert(72, TimeUnits.Seconds, TimeUnits.Hours);
+
+            Assert.AreEqual(0.02, value);
+        }
+
+        [TestMethod]
+        public void FromHToS()
+        {
+            var value = conv.Convert(0.02, TimeUnits.Hours, TimeUnits.Seconds);
+
+            Assert.AreEqual(72, value);
+        }
+
+        [TestMethod]
+        public void FromSToD()
+        {
+            var value = conv.Convert(648000, TimeUnits.Seconds, TimeUnits.Days);
+
+            Assert.AreEqual(7.5, value);
+        }
+
+        [TestMethod]
+        public void FromDToS()
+        {
+            var value = conv.Convert(7.5, TimeUnits.Days, TimeUnits.Seconds);
+
+            Assert.AreEqual(648000, value);
+        }
+
+        [TestMethod]
+        public void FromSToW()
+        {
+            var value = conv.Convert(1412812.8, TimeUnits.Seconds, TimeUnits.Weeks);
+
+            Assert.AreEqual(2.336, value);
+        }
+
+        [TestMethod]
+        public void FromWToS()
+        {
+            var value = conv.Convert(2.336, TimeUnits.Weeks, TimeUnits.Seconds);
+
+            Assert.AreEqual(1412812.8, value);
+        }
+        
     }
 
 }

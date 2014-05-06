a0e02aa11aeb802bc5b8afcaca7495fc892bc5d8
sys27 <sys2712@gmail.com>
2014-05-06 18:45:57 +0300

    Created project.

---
+++ xFunc.UnitConverters/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xFunc.UnitConverters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xFunc.UnitConverters")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if !PORTABLE
+[assembly: ComVisible(false)]
+[assembly: Guid("0c0a7e2e-fae0-4340-a298-0b31bfb5e82d")]
+#endif

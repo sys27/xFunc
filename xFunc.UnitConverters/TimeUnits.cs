da7f0849963d637711325d5514ed4f3cf0221ca1
sys_27 <sys2712@gmail.com>
2014-05-06 20:36:28 +0300

    #24 Time

---
+++ xFunc.UnitConverters/TimeUnits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public enum TimeUnits
+    {
+
+        Microseconds,
+        Milliseconds,
+        Seconds,
+        Minutes,
+        Hours,
+        Days,
+        Weeks,
+        Years
+
+    }
+
+}

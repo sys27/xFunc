8fd11737537dd5ca9cd9c5340b6c38efe267afcf
sys_27 <sys2712@gmail.com>
2014-05-06 19:55:48 +0300

    #24 Added a temperature converter.

---
+++ xFunc.UnitConverters/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public abstract class Converter<TUnit>
+    {
+
+        protected static TUnit BaseUnit;
+        protected static Dictionary<TUnit, Func<double, double>> convTo = new Dictionary<TUnit, Func<double, double>>();
+        protected static Dictionary<TUnit, Func<double, double>> convFrom = new Dictionary<TUnit, Func<double, double>>();
+
+        public double Convert(double value, TUnit from, TUnit to)
+        {
+            if (from.Equals(to))
+                return value;
+
+            var valueInBaseUnit = from.Equals(BaseUnit) ? value : convFrom[from](value);
+
+            return to.Equals(BaseUnit) ? valueInBaseUnit : convTo[to](valueInBaseUnit);
+        }
+
+        protected static void RegisterConversion(TUnit unit, Func<double, double> conversionTo, Func<double, double> conversionFrom)
+        {
+            convTo.Add(unit, conversionTo);
+            convFrom.Add(unit, conversionFrom);
+        }
+
+    }
+
+}

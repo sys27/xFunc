872764b2c4344d1b2f0dadd0cb71b29f2deee202
Dmytro Kyshchenko <sys2712@gmail.com>
2014-05-07 18:54:14 +0300

    #24 Mass

---
+++ xFunc.Test/Converters/MassTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xFunc.UnitConverters;
+
+namespace xFunc.Test.Converters
+{
+
+    [TestClass]
+    public class MassTest
+    {
+
+        private MassConverter conv = new MassConverter();
+
+        [TestMethod]
+        public void FromKiloToMilli()
+        {
+            var value = conv.Convert(12, MassUnits.Kilograms, MassUnits.Milligrams);
+
+            Assert.AreEqual(12000000, value);
+        }
+
+        [TestMethod]
+        public void FromMilliToKilo()
+        {
+            var value = conv.Convert(12000000, MassUnits.Milligrams, MassUnits.Kilograms);
+
+            Assert.AreEqual(12, value);
+        }
+
+        [TestMethod]
+        public void FromKiloToG()
+        {
+            var value = conv.Convert(12, MassUnits.Kilograms, MassUnits.Grams);
+
+            Assert.AreEqual(12000, value);
+        }
+
+        [TestMethod]
+        public void FromGToKilo()
+        {
+            var value = conv.Convert(12000, MassUnits.Grams, MassUnits.Kilograms);
+
+            Assert.AreEqual(12, value);
+        }
+
+        [TestMethod]
+        public void FromKiloToSlug()
+        {
+            var value = conv.Convert(12, MassUnits.Kilograms, MassUnits.Slugs);
+
+            Assert.AreEqual(0.822261186743533, value, 0.00001);
+        }
+
+        [TestMethod]
+        public void FromSlugToKilo()
+        {
+            var value = conv.Convert(0.822261186743533, MassUnits.Slugs, MassUnits.Kilograms);
+
+            Assert.AreEqual(12, value, 0.00001);
+        }
+
+        [TestMethod]
+        public void FromKiloToLb()
+        {
+            var value = conv.Convert(12, MassUnits.Kilograms, MassUnits.Pounds);
+
+            Assert.AreEqual(26.4554714621853, value, 0.00001);
+        }
+
+        [TestMethod]
+        public void FromLbToKilo()
+        {
+            var value = conv.Convert(26.4554714621853, MassUnits.Pounds, MassUnits.Kilograms);
+
+            Assert.AreEqual(12, value, 0.00001);
+        }
+
+        [TestMethod]
+        public void FromKiloToTonne()
+        {
+            var value = conv.Convert(12000, MassUnits.Kilograms, MassUnits.Tonne);
+
+            Assert.AreEqual(12, value, 0.00001);
+        }
+
+        [TestMethod]
+        public void FromTonneToKilo()
+        {
+            var value = conv.Convert(12, MassUnits.Tonne, MassUnits.Kilograms);
+
+            Assert.AreEqual(12000, value, 0.00001);
+        }
+
+    }
+
+}
---
+++ xFunc.UnitConverters/MassConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public class MassConverter : Converter<MassUnits>
+    {
+
+        static MassConverter()
+        {
+            BaseUnit = MassUnits.Kilograms;
+
+            RegisterConversion(MassUnits.Milligrams, m => m * 1000000, m => m / 1000000);
+            RegisterConversion(MassUnits.Grams, m => m * 1000, m => m / 1000);
+            RegisterConversion(MassUnits.Slugs, m => m / 14.593903, m => m * 14.593903);
+            RegisterConversion(MassUnits.Pounds, m => m * 2.20462262184878, m => m / 2.20462262184878);
+            RegisterConversion(MassUnits.Tonne, m => m / 1000, m => m * 1000);
+        }
+
+    }
+
+}

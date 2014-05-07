e2cf840c8d8dd4a86980601664971f5702717c03
sys_27 <sys2712@gmail.com>
2014-05-07 18:54:14 +0300

    #24 Mass

---
+++ xFunc.UnitConverters/MassUnits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+    
+    public enum MassUnits
+    {
+
+        Milligrams,
+        Grams,
+        Kilograms,
+        Slugs,
+        Pounds,
+        Tonne
+
+    }
+
+}

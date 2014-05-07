681b446ae689ec42dcba3010cc3c7ee9353b7706
Dmytro Kyshchenko <sys2712@gmail.com>
2014-05-07 19:08:36 +0300

    #24 Power

---
+++ xFunc.UnitConverters/PowerConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xFunc.UnitConverters
+{
+
+    public class PowerConverter : Converter<PowerUnits>
+    {
+
+        static PowerConverter()
+        {
+            BaseUnit = PowerUnits.Watts;
+
+            RegisterConversion(PowerUnits.Kilowatts, p => p / 1000, p => p * 1000);
+            RegisterConversion(PowerUnits.Horsepower, p => p / 745.69987158227022, p => p * 745.69987158227022);
+        }
+
+    }
+
+}

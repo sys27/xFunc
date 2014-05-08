d0e65343d9b80f6623460a0411d6c37302654aeb
Dmytro Kyshchenko <sys2712@gmail.com>
2014-05-08 10:30:39 +0300

    .

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.14.1")]
-[assembly: AssemblyFileVersion("2.14.1")]
-[assembly: AssemblyInformationalVersion("2.14.1")]
+[assembly: AssemblyVersion("2.15")]
+[assembly: AssemblyFileVersion("2.15")]
+[assembly: AssemblyInformationalVersion("2.15")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

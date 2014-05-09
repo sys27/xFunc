071daa45e847f8feeeb30ce88e6b4e32bbd40382
sys27 <sys2712@gmail.com>
2014-05-09 12:02:18 +0300

    .

--- SharedCode/Func.cs
+++ SharedCode/Func.cs
@@ -2,6 +2,7 @@
 namespace System
 {
 
+    public delegate TResult Func<out TResult>();
 	public delegate TResult Func<in T, out TResult>(T arg);
     public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
 

79d7154985ddf5f65ddd02c9d09574d18aa6d6bd
Dmytro Kyshchenko <sys2712@gmail.com>
2014-06-22 10:43:44 +0300

    Fixed translation

--- SharedCode/SharedAssemblyInfo.cs
+++ SharedCode/SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("2.15.13")]
-[assembly: AssemblyFileVersion("2.15.13")]
-[assembly: AssemblyInformationalVersion("2.15.13")]
+[assembly: AssemblyVersion("2.15.14")]
+[assembly: AssemblyFileVersion("2.15.14")]
+[assembly: AssemblyInformationalVersion("2.15.14")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

6842e257eb6a682127ad312b4b7b5ed65433b1a0
sys27 <sys2712@gmail.com>
2015-05-12 16:59:58 +0300

    .

--- xFunc.Maths/Expressions/Simplify.cs
+++ xFunc.Maths/Expressions/Simplify.cs
@@ -192,7 +192,7 @@ namespace xFunc.Maths.Expressions
         {
             get
             {
-                return ExpressionResultType.Expression;
+                return ExpressionResultType.None;
             }
         }
 

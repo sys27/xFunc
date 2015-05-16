629ebb284b7a9cae9aca9d5db80a6fae4ca5b7c2
sys27 <sys2712@gmail.com>
2015-05-16 20:00:01 +0300

    #61 Partial combined

--- xFunc/Presenters/Updater.cs
+++ xFunc/Presenters/Updater.cs
@@ -23,7 +23,7 @@ namespace xFunc.Presenters
     {
 
         private const string CheckUri = "http://xfunc.codeplex.com/releases/";
-        private const int CurrentRelease = 614830;
+        private const int CurrentRelease = 615107;
         private string updateUri;
         private bool hasUpdates;
 

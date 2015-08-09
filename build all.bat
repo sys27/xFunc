d8061c35b6a45443e78436b7affe86b6515063ad
sys27 <sys2712@gmail.com>
2015-08-09 03:47:12 +0300

    .

--- build all.bat
+++ build all.bat
@@ -3,37 +3,37 @@
 set t=%1
 if "%1"=="" set t=Build
 echo %t% xFunc for .Net 2.0 ...
-msbuild "xFunc (Libs for .Net 2.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 2.0).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed!
 
 echo %t% xFunc for .Net 3.0 ...
-msbuild "xFunc (Libs for .Net 3.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 3.0).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed! 
 
 echo %t% xFunc for .Net 3.5 ...
-msbuild "xFunc (Libs for .Net 3.5).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 3.5).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed! 
 
 echo %t% xFunc for .Net 4.0 ...
-msbuild "xFunc (Libs for .Net 4.0).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 4.0).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed!
 
 echo %t% xFunc for .Net 4.5 ...
-msbuild "xFunc (Libs for .Net 4.5).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 4.5).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed! 
 
 echo %t% xFunc for .Net 4.5.1 ...
-msbuild "xFunc (Libs for .Net 4.5.1).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for .Net 4.5.1).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed! 
 
 echo %t% xFunc for Portable ...
-msbuild "xFunc (Libs for Portable).sln" /nologo /t:%t% /m /clp:ErrorsOnly /p:Configuration=Release
+msbuild "xFunc (Libs for Portable).sln" /nologo /t:%t% /m /clp:WarningsOnly;ErrorsOnly /p:Configuration=Release
 if errorlevel 1 goto end
 if errorlevel 0 echo %t% completed! 
 

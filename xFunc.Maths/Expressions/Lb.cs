ea0a716acd0c8cc5b7ef1ed9a9a92484ed9ba733
Dmytro Kyshchenko <sys2712@gmail.com>
2015-11-19 22:19:13 +0200

    Add #114

---
+++ xFunc.Maths/Expressions/Lb.cs
@@ -0,0 +1,79 @@
+﻿// Copyright 2012-2015 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+
+namespace xFunc.Maths.Expressions
+{
+
+    /// <summary>
+    /// Represents the binary logarithm function.
+    /// </summary>
+    public class Lb : UnaryExpression
+    {
+
+        internal Lb() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Lb"/> class.
+        /// </summary>
+        /// <param name="expression">The argument of function.</param>
+        /// <seealso cref="IExpression"/>
+        public Lb(IExpression expression) : base(expression) { }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode(8807);
+        }
+
+        /// <summary>
+        /// Converts this expression to the equivalent string.
+        /// </summary>
+        /// <returns>The string that represents this expression.</returns>
+        public override string ToString()
+        {
+            return ToString("lb({0})");
+        }
+
+        /// <summary>
+        /// Calculates this Ln expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the calculation.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        public override object Calculate(ExpressionParameters parameters)
+        {
+            return Math.Log((double)m_argument.Calculate(parameters), 2);
+        }
+
+        /// <summary>
+        /// Clones this instance of the <see cref="Lb"/>.
+        /// </summary>
+        /// <returns>Returns the new instance of <see cref="IExpression"/> that is a clone of this instance.</returns>
+        public override IExpression Clone()
+        {
+            return new Lb(m_argument.Clone());
+        }
+
+    }
+
+}

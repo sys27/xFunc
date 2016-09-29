85a55e326d96858aa051e5a93f7f2a088087cbc7
sys27 <sys2712@gmail.com>
2016-09-29 18:20:11 +0300

    Resolver

---
+++ xFunc.Maths/DefaultDependencyResolver.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2012-2016 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+
+namespace xFunc.Maths
+{
+
+    /// <summary>
+    /// The default implementation of methods to resolve object for post parse process.
+    /// </summary>
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+
+        private Container container;
+
+        public DefaultDependencyResolver()
+        {
+            container = new Container();
+        }
+
+        public DefaultDependencyResolver(object[] objects)
+        {
+            container = new Container();
+
+            if (objects != null)
+                foreach (var obj in objects)
+                    container.RegisterSingleton(obj.GetType(), obj);
+        }
+
+        public object Resolve(Type type)
+        {
+            return container.GetInstance(type);
+        }
+
+        public T Resolve<T>() where T: class
+        {
+            return container.GetInstance<T>();
+        }
+
+    }
+
+}

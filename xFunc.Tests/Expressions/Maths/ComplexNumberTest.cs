b704afeddb48c45f696e6c0efc6eeef75e7f0a44
Dmytro Kyshchenko <sys2712@gmail.com>
2016-10-19 02:16:39 +0300

    Tests

---
+++ xFunc.Tests/Expressions/Maths/ComplexNumberTest.cs
@@ -0,0 +1,73 @@
+﻿// Copyright 2012-2016 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Numerics;
+using xFunc.Maths.Expressions;
+using Xunit;
+
+namespace xFunc.Tests.Expressions.Maths
+{
+
+    public class ComplexNumberTest
+    {
+
+        [Fact]
+        public void ExecuteTest()
+        {
+            var complex = new Complex(5, 2);
+            var exp = new ComplexNumber(complex);
+
+            Assert.Equal(complex, exp.Execute());
+        }
+
+        [Fact]
+        public void ExecuteWithParamsTest()
+        {
+            var complex = new Complex(5, 2);
+            var exp = new ComplexNumber(complex);
+
+            Assert.Equal(complex, exp.Execute(null));
+        }
+
+        [Fact]
+        public void ValueTest()
+        {
+            var complex = new Complex(5, 2);
+            var exp = new ComplexNumber(complex);
+
+            Assert.Equal(complex, exp.Value);
+        }
+
+        [Fact]
+        public void EqualsTest()
+        {
+            var exp1 = new ComplexNumber(new Complex(5, 2));
+            var exp2 = new ComplexNumber(new Complex(5, 2));
+
+            Assert.True(exp1.Equals(exp2));
+        }
+
+        [Fact]
+        public void NotEqualsTest()
+        {
+            var exp1 = new ComplexNumber(new Complex(5, 2));
+            var exp2 = new ComplexNumber(new Complex(3, 2));
+
+            Assert.False(exp1.Equals(exp2));
+        }
+
+    }
+
+}

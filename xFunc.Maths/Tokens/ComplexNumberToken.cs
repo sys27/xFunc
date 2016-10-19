edbe7db2c00b9626983f198b98ee72628427dd45
sys_27 <sys2712@gmail.com>
2016-10-19 13:40:37 +0300

    #147

---
+++ xFunc.Maths/Tokens/ComplexNumberToken.cs
@@ -0,0 +1,68 @@
+﻿// Copyright 2012-2016 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Numerics;
+
+namespace xFunc.Maths.Tokens
+{
+
+    public class ComplexNumberToken : IToken
+    {
+
+        private readonly Complex complex;
+
+        public ComplexNumberToken(Complex complex)
+        {
+            this.complex = complex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var num = obj as ComplexNumberToken;
+            if (num == null)
+                return false;
+
+            return complex.Equals(num.complex);
+        }
+
+        public override int GetHashCode()
+        {
+            return complex.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Complex Number: {complex}";
+        }
+
+        public int Priority
+        {
+            get
+            {
+                return 101;
+            }
+        }
+
+        public Complex Number
+        {
+            get
+            {
+                return complex;
+            }
+        }
+
+    }
+
+}

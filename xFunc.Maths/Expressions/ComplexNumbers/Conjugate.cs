fed6698ded849b4909277ecbeb658d0b32ac6392
sys27 <sys2712@gmail.com>
2016-10-21 16:53:05 +0300

    #147 conjugate

---
+++ xFunc.Maths/Expressions/ComplexNumbers/Conjugate.cs
@@ -0,0 +1,115 @@
+﻿// Copyright 2012-2016 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Numerics;
+
+namespace xFunc.Maths.Expressions.ComplexNumbers
+{
+
+    /// <summary>
+    /// Respresents the Conjugate function.
+    /// </summary>
+    /// <seealso cref="xFunc.Maths.Expressions.UnaryExpression" />
+    public class Conjugate : UnaryExpression
+    {
+
+        internal Conjugate() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Conjugate"/> class.
+        /// </summary>
+        /// <param name="argument">The expression.</param>
+        public Conjugate(IExpression argument)
+            : base(argument)
+        {
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode(2719);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return base.ToString("conjugate({0})");
+        }
+
+        /// <summary>
+        /// Executes this expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the execution.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
+        public override object Execute(ExpressionParameters parameters)
+        {
+            return Complex.Conjugate((Complex)m_argument.Execute(parameters));
+        }
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="IExpression" /> that is a clone of this instance.
+        /// </returns>
+        public override IExpression Clone()
+        {
+            return new Conjugate(m_argument.Clone());
+        }
+
+        /// <summary>
+        /// Gets the type of the argument.
+        /// </summary>
+        /// <value>
+        /// The type of the argument.
+        /// </value>
+        public override ExpressionResultType ArgumentType
+        {
+            get
+            {
+                return ExpressionResultType.ComplexNumber;
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the result.
+        /// </summary>
+        /// <value>
+        /// The type of the result.
+        /// </value>
+        public override ExpressionResultType ResultType
+        {
+            get
+            {
+                return ExpressionResultType.ComplexNumber;
+            }
+        }
+
+    }
+
+}

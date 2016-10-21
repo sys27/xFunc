b4e82c71e8f74abe69d35cf5365470308efd77fc
sys27 <sys2712@gmail.com>
2016-10-21 17:00:52 +0300

    #147 reciprocal and docs

--- xFunc.Maths/Expressions/ComplexNumbers/ComplexNumber.cs
+++ xFunc.Maths/Expressions/ComplexNumbers/ComplexNumber.cs
@@ -24,26 +24,56 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
         private IExpression parent;
         private Complex complex;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComplexNumber"/> class.
+        /// </summary>
+        /// <param name="real">The real part.</param>
+        /// <param name="imaginary">The imaginary part.</param>
         public ComplexNumber(double real, double imaginary)
             : this(new Complex(real, imaginary))
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComplexNumber"/> class.
+        /// </summary>
+        /// <param name="complex">The complex number.</param>
         public ComplexNumber(Complex complex)
         {
             this.complex = complex;
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="ComplexNumber"/> to <see cref="Complex"/>.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator Complex(ComplexNumber number)
         {
             return number.complex;
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="Complex"/> to <see cref="ComplexNumber"/>.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ComplexNumber(Complex number)
         {
             return new ComplexNumber(number);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             var num = obj as ComplexNumber;
@@ -53,31 +83,69 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             return complex.Equals(num.complex);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return complex.GetHashCode() ^ 6421;
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return complex.ToString();
         }
 
+        /// <summary>
+        /// Executes this expression. Don't use this method if your expression has variables or user-functions.
+        /// </summary>
+        /// <returns>
+        /// A result of the execution.
+        /// </returns>
         public object Execute()
         {
             return complex;
         }
 
+        /// <summary>
+        /// Executes this expression.
+        /// </summary>
+        /// <param name="parameters">An object that contains all parameters and functions for expressions.</param>
+        /// <returns>
+        /// A result of the execution.
+        /// </returns>
+        /// <seealso cref="ExpressionParameters" />
         public object Execute(ExpressionParameters parameters)
         {
             return complex;
         }
 
+        /// <summary>
+        /// Clones this instance of the <see cref="IExpression" />.
+        /// </summary>
+        /// <returns>
+        /// Returns the new instance of <see cref="IExpression" /> that is a clone of this instance.
+        /// </returns>
         public IExpression Clone()
         {
             return new ComplexNumber(this.complex);
         }
 
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         public Complex Value
         {
             get
@@ -86,6 +154,9 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             }
         }
 
+        /// <summary>
+        /// Get or Set the parent expression.
+        /// </summary>
         public IExpression Parent
         {
             get
@@ -99,6 +170,12 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             }
         }
 
+        /// <summary>
+        /// Gets the minimum count of parameters.
+        /// </summary>
+        /// <value>
+        /// The minimum count of parameters.
+        /// </value>
         public int MinParameters
         {
             get
@@ -107,6 +184,12 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             }
         }
 
+        /// <summary>
+        /// Gets the maximum count of parameters.
+        /// </summary>
+        /// <value>
+        /// The maximum count of parameters.
+        /// </value>
         public int MaxParameters
         {
             get
@@ -115,6 +198,12 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             }
         }
 
+        /// <summary>
+        /// Gets the count of parameters.
+        /// </summary>
+        /// <value>
+        /// The count of parameters.
+        /// </value>
         public int ParametersCount
         {
             get
@@ -123,6 +212,12 @@ namespace xFunc.Maths.Expressions.ComplexNumbers
             }
         }
 
+        /// <summary>
+        /// Gets the type of the result.
+        /// </summary>
+        /// <value>
+        /// The type of the result.
+        /// </value>
         public ExpressionResultType ResultType
         {
             get

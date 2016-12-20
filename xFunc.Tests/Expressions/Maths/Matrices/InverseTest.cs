bc130270b459e31cbdce58ccfc410580867c01cb
sys_27 <sys2712@gmail.com>
2016-12-20 12:16:26 +0200

    #95

--- xFunc.Tests/Expressions/Maths/Matrices/InverseTest.cs
+++ xFunc.Tests/Expressions/Maths/Matrices/InverseTest.cs
@@ -58,6 +58,20 @@ namespace xFunc.Tests.Expressions.Maths.Matrices
                 })));
         }
 
+        [Fact]
+        public void ToStringTest()
+        {
+            var matrix = new Matrix(new[]
+            {
+                new Vector(new[] { new Number(1), new Number(-2) }),
+                new Vector(new[] { new Number(4), new Number(0) })
+            });
+
+            var exp = new Inverse(matrix);
+
+            Assert.Equal("inverse({{1, -2}, {4, 0}})", exp.ToString());
+        }
+
     }
 
 }

b16d355bbdf3dbe6dc24d1969c54e4da14e0f6c7
sys_27 <sys2712@gmail.com>
2016-12-29 12:41:18 +0200

    Removed Converage Attribute

--- xFunc.Maths/Analyzers/Analyzer.cs
+++ xFunc.Maths/Analyzers/Analyzer.cs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 using System;
+using System.Diagnostics.CodeAnalysis;
 using xFunc.Maths.Expressions;
 using xFunc.Maths.Expressions.ComplexNumbers;
 using xFunc.Maths.Expressions.Hyperbolic;

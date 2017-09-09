dece97888eb010f1fe44de54aa844c4b220cdea5
Dmytro Kyshchenko <sys2712@gmail.com>
2017-09-09 17:31:08 +0300

    Fix docs.

--- xFunc.Maths/Analyzers/TypeAnalyzers/BinaryParameterTypeMismatchException.cs
+++ xFunc.Maths/Analyzers/TypeAnalyzers/BinaryParameterTypeMismatchException.cs
@@ -19,18 +19,40 @@ using xFunc.Maths.Resources;
 namespace xFunc.Maths.Analyzers.TypeAnalyzers
 {
 
+    /// <summary>
+    /// Type of binary parameter.
+    /// </summary>
     public enum BinaryParameterType
     {
+        /// <summary>
+        /// The left parameter.
+        /// </summary>
         Left,
+        /// <summary>
+        /// The right parameter.
+        /// </summary>
         Right
     }
 
+    /// <summary>
+    /// Represents an exception when the type of the actual argument does not match the expected parameter type.
+    /// </summary>
+    /// <seealso cref="xFunc.Maths.Analyzers.TypeAnalyzers.ParameterTypeMismatchException" />
     [Serializable]
     public class BinaryParameterTypeMismatchException : ParameterTypeMismatchException
     {
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryParameterTypeMismatchException"/> class.
+        /// </summary>
         public BinaryParameterTypeMismatchException() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryParameterTypeMismatchException"/> class.
+        /// </summary>
+        /// <param name="expected">The expected.</param>
+        /// <param name="actual">The actual.</param>
+        /// <param name="parameterType">Type of the parameter.</param>
         public BinaryParameterTypeMismatchException(
             ResultType expected,
             ResultType actual,
@@ -42,12 +64,28 @@ namespace xFunc.Maths.Analyzers.TypeAnalyzers
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryParameterTypeMismatchException"/> class.
+        /// </summary>
+        /// <param name="expected">The expected parameter type.</param>
+        /// <param name="actual">The actual parameter type.</param>
+        /// <param name="message">The error message.</param>
         public BinaryParameterTypeMismatchException(ResultType expected, ResultType actual, string message) : base(expected, actual, message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryParameterTypeMismatchException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
         public BinaryParameterTypeMismatchException(string message, Exception inner) : base(message, inner) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinaryParameterTypeMismatchException"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
         protected BinaryParameterTypeMismatchException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
     }

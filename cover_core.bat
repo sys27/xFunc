ad0210157c2e06c8d039c4368732b7628d7a7c3f
sys_27 <sys2712@gmail.com>
2017-09-23 21:06:23 +0300

    #204

--- cover_core.bat
+++ cover_core.bat
@@ -1,3 +1,3 @@
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -output:coverage.xml -target:"dotnet.exe" -targetargs:"test xFunc.Tests\xFunc.Tests.Core.csproj" -excludebyattribute:*.ExcludeFromCodeCoverage*^ -filter:"+[xFunc.*]* -[xFunc.Tests*]* -[xFunc.*]*.Resource -[xFunc.*]*Exception"
-packages\ReportGenerator.3.0.0\tools\ReportGenerator.exe coverage.xml .\coverage
+%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe -register:user -output:coverage.xml -target:"dotnet.exe" -targetargs:"test xFunc.Tests\xFunc.Tests.Core.csproj --no-build" -excludebyattribute:*.ExcludeFromCodeCoverage*^ -filter:"+[xFunc.*]* -[xFunc.Tests*]* -[xFunc.*]*.Resource -[xFunc.*]*Exception"
+%USERPROFILE%\.nuget\packages\ReportGenerator\3.0.0\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:coverage
 pause

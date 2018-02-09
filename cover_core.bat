389520b8b866e091a2616ef6f1709ffcc575d508
sys_27 <sys2712@gmail.com>
2018-02-10 00:15:10 +0200

    Update packages

--- cover_core.bat
+++ cover_core.bat
@@ -1,3 +1,3 @@
 %USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe -register:user -output:coverage.xml -target:"dotnet.exe" -targetargs:"test xFunc.Tests\xFunc.Tests.Core.csproj --no-build -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage*^ -filter:"+[xFunc.*]* -[xFunc.Tests*]* -[xFunc.*]*.Resource -[xFunc.*]*Exception" -oldStyle
-%USERPROFILE%\.nuget\packages\ReportGenerator\3.0.0\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:coverage
+%USERPROFILE%\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:coverage
 pause

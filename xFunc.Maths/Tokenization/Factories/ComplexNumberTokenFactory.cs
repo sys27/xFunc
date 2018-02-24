85cde01ac79cb31ba8f71dcaca30c72e94e4a8ad
Dmytro Kyshchenko <sys2712@gmail.com>
2018-02-24 14:47:38 +0200

    #206

---
+++ xFunc.Maths/Tokenization/Factories/ComplexNumberTokenFactory.cs
@@ -0,0 +1,49 @@
+// Copyright 2012-2018 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using xFunc.Maths.Tokenization.Tokens;
+
+namespace xFunc.Maths.Tokenization.Factories
+{
+    public class ComplexNumberTokenFactory : FactoryBase
+    {
+        private Regex regexAllWhitespaces = new Regex(@"\s+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public ComplexNumberTokenFactory() : base(new Regex(@"\G([+-]?\s*\d*\.?\d+)\s*([∠+-]+\s*\s*\d*\.?\d+)°", RegexOptions.Compiled | RegexOptions.IgnoreCase)) { }
+
+        private bool DoubleTryParse(string str, out double number)
+        {
+            return double.TryParse(str, NumberStyles.Number, CultureInfo.InvariantCulture, out number);
+        }
+
+        protected override FactoryResult CreateTokenInternal(Match match, IReadOnlyList<IToken> tokens)
+        {
+            string magnitudeString = regexAllWhitespaces.Replace(match.Groups[1].Value, string.Empty);
+            if (!DoubleTryParse(magnitudeString, out double magnitude))
+                magnitude = 0.0;
+
+            string phaseString = regexAllWhitespaces.Replace(match.Groups[2].Value, string.Empty).Replace("∠", "");
+            if (!DoubleTryParse(phaseString, out double phase))
+                phase = 1.0;
+
+            var token = new ComplexNumberToken(Complex.FromPolarCoordinates(magnitude, phase * Math.PI / 180));
+            return new FactoryResult(token, match.Length);
+        }
+    }
+}
---
+++ xFunc.Maths/Tokenization/Factories/NumberBinTokenFactory.cs
@@ -0,0 +1,33 @@
+// Copyright 2012-2018 Dmitry Kischenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+// express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using xFunc.Maths.Tokenization.Tokens;
+
+namespace xFunc.Maths.Tokenization.Factories
+{
+    public class NumberBinTokenFactory : FactoryBase
+    {
+        public NumberBinTokenFactory() : base(new Regex(@"\G[+-]?0b[01]+", RegexOptions.Compiled | RegexOptions.IgnoreCase)) { }
+
+        protected override FactoryResult CreateTokenInternal(Match match, IReadOnlyList<IToken> tokens)
+        {
+            var token = new NumberToken(Convert.ToInt64(match.Value.Replace("0b", ""), 2));
+
+            return new FactoryResult(token, match.Length);
+        }
+    }
+}

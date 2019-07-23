aa1ae88555e494b955a9282a03d5840712c9424c
sys27 <sys2712@gmail.com>
2019-07-23 21:52:38 +0300

    #228 - Introduce benchmark

---
+++ xFunc.Benchmark/Benchmarks/ProcessorBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using xFunc.Maths;
+using xFunc.Maths.Expressions;
+
+namespace xFunc.Benchmark.Benchmarks
+{
+    public class ProcessorBenchmark
+    {
+        private Processor processor;
+
+        [Params(10, 100, 1000)]
+        public int Iterations;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            processor = new Processor();
+        }
+
+        [Benchmark]
+        public void Parse()
+        {
+            IExpression exp = null;
+            for (var i = 0; i < Iterations; i++)
+                exp = processor.Parse("(100.1 + (sin(cos(tan(ctg(x)))) * 3) / (func(a, b, c) ^ 2)) - (cos(y) - 111.3) & (true | false -> true <-> false) + (det({{1, 2}, {3, 4}}) * log(2, 3))");
+        }
+    }
+}
---
+++ xFunc.Benchmark/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace xFunc.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner
+                .Run(typeof(Program).Assembly,
+                     ManualConfig.Create(DefaultConfig.Instance)
+                                 .With(Job.MediumRun
+                                          .WithLaunchCount(1)
+                                          .With(CsProjCoreToolchain.NetCoreApp22))
+                                 .With(MemoryDiagnoser.Default)
+                                 .StopOnFirstError()
+                );
+        }
+    }
+}

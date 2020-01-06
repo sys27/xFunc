35b22a8e99ff21dc0f49150a2caebe88c2044b35
Dmytro Kyshchenko <sys2712@gmail.com>
2020-01-06 14:38:24 +0200

    Update copyright

--- xFunc.Maths/Expressions/Ceil.cs
+++ xFunc.Maths/Expressions/Ceil.cs
@@ -1,4 +1,5 @@
 ﻿// Copyright 2014 Ronny Weidemann
+// Copyright 2014-2020 Dmytro Kyshchenko
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License.
--- xFunc.Tests/Expressions/Hyperbolic/HyperbolicArsecantTest.cs
+++ xFunc.Tests/Expressions/Hyperbolic/HyperbolicArsecantTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2012-2019 Dmitry Kischenko
+﻿// Copyright 2012-2020 Dmytro Kyshchenko
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License.

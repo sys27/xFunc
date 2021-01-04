108122251d5096598a799c6b58d1ecdd2fac7fec
Dmytro Kyshchenko <sys2712@gmail.com>
2021-01-04 21:00:45 +0200

    Update copyright (#393)

--- xFunc.Maths/Expressions/Ceil.cs
+++ xFunc.Maths/Expressions/Ceil.cs
@@ -1,5 +1,5 @@
 // Copyright 2014 Ronny Weidemann
-// Copyright 2014-2020 Dmytro Kyshchenko
+// Copyright 2014-2021 Dmytro Kyshchenko
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

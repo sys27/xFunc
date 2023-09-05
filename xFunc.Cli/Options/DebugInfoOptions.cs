315f5210dde89255eb554acf737b8c3e51f89b4b
Dmytro Kyshchenko <sys2712@gmail.com>
2023-09-05 20:47:05 +0300

    #710 - Add ability to execute expression from files.

--- xFunc.Cli/Options/DebugInfoOptions.cs
+++ xFunc.Cli/Options/DebugInfoOptions.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Dmytro Kyshchenko. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace xFunc.DotnetTool.Options;
+namespace xFunc.Cli.Options;
 
 public abstract class DebugInfoOptions
 {
--- xFunc.Cli/Options/ParseOptions.cs
+++ xFunc.Cli/Options/ParseOptions.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Dmytro Kyshchenko. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace xFunc.DotnetTool.Options;
+namespace xFunc.Cli.Options;
 
 [Verb("parse", HelpText = "Parse string expression.")]
 public class ParseOptions : BaseOptions
